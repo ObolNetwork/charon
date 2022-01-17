62e5ee99c72b6ebd21a65666e11def515bd042e3
dB2510 <dhruvbodani2510@gmail.com>
2022-01-18 01:24:35 +0530

    OBOL-148: create TBLS keyshares

--- crypto/tbls_test.go
+++ crypto/tbls_test.go
@@ -1,13 +1,15 @@
 package crypto
 
 import (
+	"testing"
+
 	"github.com/prysmaticlabs/prysm/v2/beacon-chain/core/signing"
 	"github.com/prysmaticlabs/prysm/v2/encoding/bytesutil"
 	ethpb "github.com/prysmaticlabs/prysm/v2/proto/eth/v1"
-	"testing"
 )
 
 func TestTBLSAggregation(t *testing.T) {
+	// Create Attestation Data Object
 	beaconBlockRoot := bytesutil.ToBytes32([]byte("A"))
 	targetRoot := bytesutil.ToBytes32([]byte("B"))
 	sourceRoot := bytesutil.ToBytes32([]byte("C"))
@@ -16,9 +18,23 @@ func TestTBLSAggregation(t *testing.T) {
 		Source:          &ethpb.Checkpoint{Root: sourceRoot[:], Epoch: 3},
 		Target:          &ethpb.Checkpoint{Root: targetRoot[:]},
 	}
+
 	signingRoot, err := signing.ComputeSigningRoot(attestationData, make([]byte, 32))
 	if err != nil {
 		t.Fatal("Error in signing root: ", err)
+	}
+
+	// Creating Threshold BLS Key shares
+	threshold := 3
+	numberOfShares := 5
+	priPoly, pubPoly := NewTBLSPoly(uint(threshold))
+	priKeyShares := priPoly.Shares(numberOfShares)
+	pubKeyShares := pubPoly.Shares(numberOfShares)
+	for _, priKeyShare := range priKeyShares {
+		t.Log("Private key share: ", *priKeyShare)
+	}
+	for _, pubKeyShare := range pubKeyShares {
+		t.Log("Public key share: ", *pubKeyShare)
 	}
 	t.Log("Signing Root: ", signingRoot)
 }
