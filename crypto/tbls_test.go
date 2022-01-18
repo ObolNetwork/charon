eaec04fba4cdbe50a3685f7cee501224c5a6562c
dB2510 <dhruvbodani2510@gmail.com>
2022-01-19 04:31:32 +0530

    add test to sign, aggregate and verify attestations

--- crypto/tbls_test.go
+++ crypto/tbls_test.go
@@ -3,9 +3,12 @@ package crypto
 import (
 	"testing"
 
+	"github.com/prysmaticlabs/prysm/v2/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/v2/beacon-chain/core/signing"
+	"github.com/prysmaticlabs/prysm/v2/crypto/bls"
 	"github.com/prysmaticlabs/prysm/v2/encoding/bytesutil"
-	ethpb "github.com/prysmaticlabs/prysm/v2/proto/eth/v1"
+	ethpb "github.com/prysmaticlabs/prysm/v2/proto/prysm/v1alpha1"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,16 +29,33 @@ func TestTBLSAggregation(t *testing.T) {
 	// Creating Threshold BLS Key shares
 	threshold := 3
 	numberOfShares := 5
-	priPoly, pubPoly := NewTBLSPoly(threshold)
-	priKeyShares := priPoly.Shares(numberOfShares)
-	pubKeyShares := pubPoly.Shares(numberOfShares)
-	for _, priKeyShare := range priKeyShares {
-		t.Log("Private key share: ", *priKeyShare)
-	}
-	for _, pubKeyShare := range pubKeyShares {
-		t.Log("Public key share: ", *pubKeyShare)
-	}
-	t.Log("Signing Root: ", signingRoot)
+	t.Run("Sign and verify attestations", func(tt *testing.T) {
+		priPoly, _ := NewTBLSPoly(threshold)
+		priKeyShares := priPoly.Shares(numberOfShares)
+		atts := make([]*ethpb.Attestation, 0, numberOfShares)
+		pubKeys := make([]bls.PublicKey, 0, numberOfShares)
+
+		for _, priKeyShare := range priKeyShares {
+			priKeyBinary, err := priKeyShare.V.MarshalBinary()
+			require.NoError(t, err)
+
+			priKey, err := bls.SecretKeyFromBytes(priKeyBinary)
+			require.NoError(t, err)
+
+			pubkey := priKey.PublicKey()
+			pubKeys = append(pubKeys, pubkey)
+
+			sig := priKey.Sign(signingRoot[:])
+			att := &ethpb.Attestation{
+				Data:      attestationData,
+				Signature: sig.Marshal(),
+			}
+			atts = append(atts, att)
+		}
+		aggSig, err := helpers.AggregateSignature(atts)
+		require.NoError(tt, err)
+		assert.Equal(tt, true, aggSig.FastAggregateVerify(pubKeys, signingRoot))
+	})
 
 	// Next steps would be:
 	// 1. Sign the signing root with different key shares with all possible
