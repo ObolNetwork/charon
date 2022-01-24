faa56741f61881c43477ba813bfedcbfe9c935b5
Dhruv Bodani <dhruv@Dhruvs-MacBook-Pro.local>
2022-01-24 13:12:53 +0530

    OBOL-209: add new version cmd

---
+++ cmd/cmd.go
@@ -0,0 +1,137 @@
+// Copyright © 2021 Obol Technologies Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cmd implements Charon's command-line interface.
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	// The name of our config file, without the file extension because
+	// viper supports many different config file languages.
+	defaultConfigFilename = ""
+
+	// The environment variable prefix of all environment variables bound to our command line flags.
+	envPrefix = "charon"
+)
+
+// New returns a new root cobra command that handles our command line tool.
+func New() *cobra.Command {
+	// Create the root command
+	root := &cobra.Command{
+		Use:   "charon",
+		Short: "Charon - The Ethereum DVT middleware client",
+		Long:  `Charon enables the operation of Ethereum validators in a fault tolerant manner by splitting the validating keys across a group of trusted parties using threshold cryptography.`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			// You can bind cobra and viper in a few locations,
+			// but PersistencePreRunE on the root command works well
+			return initializeConfig(cmd)
+		},
+	}
+
+	// Add sub-commands
+	root.AddCommand(
+		newVersionCmd(),
+	)
+
+	return root
+}
+
+// initializeConfig sets up the general viper config and bind the cobra flag to the viper flags.
+func initializeConfig(cmd *cobra.Command) error {
+	v := viper.New()
+
+	// Set the base name of the config file, without the file extension.
+	v.SetConfigName(defaultConfigFilename)
+
+	// Set as many paths as you like where viper should look for the
+	// config file. We are only looking in the current working directory.
+	v.AddConfigPath(".")
+
+	// Attempt to read the config file, gracefully ignoring errors
+	// caused by a config file not being found. Return an error
+	// if we cannot parse the config file.
+	if err := v.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	// When we bind flags to environment variables expect that the
+	// environment variables are prefixed. This helps avoid conflicts.
+	v.SetEnvPrefix(envPrefix)
+
+	// Bind to environment variables
+	// Works great for simple config names, but needs help for names
+	// like --favorite-color which we fix in the bindFlags function
+	v.AutomaticEnv()
+
+	// Bind the current command's flags to viper
+	if err := bindFlags(cmd, v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Bind each cobra flag to its associated viper configuration (config file and environment variable)
+func bindFlags(cmd *cobra.Command, v *viper.Viper) error {
+	var lastErr error
+
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores, e.g. --foo-bar to STING_FOO_BAR
+		if strings.Contains(f.Name, "-") {
+			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
+			err := v.BindEnv(f.Name, fmt.Sprintf("%s_%s", envPrefix, envVarSuffix))
+			if err != nil {
+				lastErr = err
+			}
+		}
+
+		// Cobra provided flags take priority
+		if f.Changed {
+			return
+		}
+
+		// Define all the viper flag names to check
+		viperNames := []string{
+			f.Name,
+			strings.ReplaceAll(f.Name, "_", "."), // TOML uses "." to indicate hierarchy, while we use "_" in this example.
+		}
+
+		for _, name := range viperNames {
+			if !v.IsSet(name) {
+				continue
+			}
+
+			val := v.Get(name)
+			err := cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+			if err != nil {
+				lastErr = err
+			}
+			break
+		}
+	})
+
+	return lastErr
+}
---
+++ cmd/new_version.go
@@ -0,0 +1,57 @@
+// Copyright © 2021 Obol Technologies Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	dbg "runtime/debug"
+
+	"github.com/obolnetwork/charon/internal"
+	"github.com/obolnetwork/charon/internal/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// newVersionCmd returns the version command
+func newVersionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print version and exit",
+		Long:  "Output version info. Use --verbose for detailed module version info.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(internal.ReleaseVersion)
+			if viper.GetBool(config.KeyVerbose) {
+				printNewBuildInfo()
+			}
+		},
+	}
+	return cmd
+}
+
+func printNewBuildInfo() {
+	buildInfo, ok := dbg.ReadBuildInfo()
+	if !ok {
+		fmt.Println("Failed to gather build info")
+		return
+	}
+	fmt.Printf("Package: %s\n", buildInfo.Path)
+	fmt.Println("Dependencies:")
+	for _, dep := range buildInfo.Deps {
+		for dep.Replace != nil {
+			dep = dep.Replace
+		}
+		fmt.Printf("\t%v %v\n", dep.Path, dep.Version)
+	}
+}
