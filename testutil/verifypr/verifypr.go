de08688b7cc35c636af04dcacb17c867bc165cd8
corverroos <corver.roos@gmail.com>
2022-03-17 13:52:28 +0200

    cleanup

--- testutil/verifypr/verifypr.go
+++ testutil/verifypr/verifypr.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Command verifypr provides a tool to verify charon PRs against the template defined in docs/contibuting.md.
+//
 //nolint:wrapcheck,revive,gocognit,cyclop,nestif,forbidigo
 package main
 
