6a65dfcc2a16fc01dfa776e63a53deedb5f51a65
corverroos <corver.roos@gmail.com>
2022-04-29 11:00:52 +0200

    cleanup

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -19,10 +19,10 @@
 // the output of the `charon dkg` and `charon create cluster` commands. `Lock` is also the input
 // to `charon run` command.
 //
-//          LaunchPad ─┐
-//                     ├─► cluster_definition.json ──► charon dkg ─┐
-//  charon create dkg ─┘                                       ├─► cluster_lock.json ──► charon run
-//                                      charon create cluster ─┘
+//  launchpad.obol.net ─┐
+//                      ├─► cluster_definition.json ──► charon dkg ─┐
+//   charon create dkg ─┘                                           ├─► cluster_lock.json ──► charon run
+//                                           charon create cluster ─┘
 package cluster
 
 import (
