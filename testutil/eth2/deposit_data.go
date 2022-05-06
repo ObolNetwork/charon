ad505eecdfc5503dd76027f010c2b70de3d7cba9
xenowits <kumar.abhishek111b@gmail.com>
2022-05-06 12:11:17 +0530

    add functions for hash tree root

--- testutil/eth2/deposit_data.go
+++ testutil/eth2/deposit_data.go
@@ -1,9 +1,10 @@
 package eth2
 
 import (
+	ssz "github.com/ferranbt/fastssz"
+
 	"github.com/obolnetwork/charon/app/errors"
 	"github.com/obolnetwork/charon/cluster"
-	"github.com/prysmaticlabs/prysm/v2/beacon-chain/core/signing"
 )
 
 // DepositData contains all the information required for activating validators on the Ethereum Network.
@@ -18,14 +19,6 @@ type DepositData struct {
 	NetworkName           string `json:"network_name"`
 }
 
-// DepositMessage contains the basic information necessary to activate a validator. The fields are
-// hashed to get the DepositMessageRoot. This root is signed and the signature is added to DepositData.
-type DepositMessage struct {
-	PubKey                string `json:"pubkey"`
-	Amount                uint64 `json:"amount"`
-	WithdrawalCredentials string `json:"withdrawal_credentials"`
-}
-
 // GenerateDepositData generates a deposit data object by populating the required fields.
 func GenerateDepositData(def cluster.Definition, pubkey string) (DepositData, error) {
 	amount := uint64(32000000000) // 32 Ether in Gwei
@@ -34,14 +27,7 @@ func GenerateDepositData(def cluster.Definition, pubkey string) (DepositData, er
 		Amount:                amount,
 		WithdrawalCredentials: def.WithdrawalAddress,
 	}
-
-	domainType := [4]byte{1, 2, 3, 4}
-	domain, err := signing.ComputeDomain(domainType, nil, nil)
-	depositMessageRoot, err := signing.ComputeSigningRoot(depositMessage, domain)
-	if err != nil {
-		return DepositData{}, errors.Wrap(err, "compute domain")
-	}
-
+	depositMessageRoot := ""
 	depositDataRoot := ""
 	signature := "" // sign the depositMsgRoot with the bls key
 
@@ -76,4 +62,51 @@ func forkVersionToNetwork(forkVersion string) string {
 	default:
 		return "mainnet"
 	}
+}
+
+func (d DepositData) HashTreeRoot() ([32]byte, error) {
+	b, err := ssz.HashWithDefaultHasher(d)
+	if err != nil {
+		return [32]byte{}, errors.Wrap(err, "hash deposit data")
+	}
+
+	return b, nil
+}
+
+func (d DepositData) HashTreeRootWith(hh *ssz.Hasher) error {
+	idx := hh.Index()
+
+	// Field 0 'PubKey`
+	hh.PutBytes([]byte(d.PubKey))
+
+	// Field 1 'Amount'
+	hh.PutUint64(d.Amount)
+
+	// Field 2 'WithdrawalCredentials'
+	hh.PutBytes([]byte(d.WithdrawalCredentials))
+
+	// Field 3 'DepositDataRoot'
+	if len(d.DepositDataRoot) != 32 {
+		return errors.Wrap(ssz.ErrBytesLength, "deposit data root")
+	}
+	hh.PutBytes([]byte(d.DepositDataRoot))
+
+	// Field 4 'DepositMessageRoot'
+	if len(d.DepositMessageRoot) != 32 {
+		return errors.Wrap(ssz.ErrBytesLength, "deposit message root")
+	}
+	hh.PutBytes([]byte(d.DepositMessageRoot))
+
+	// Field 5 'Signature'
+	hh.PutBytes([]byte(d.Signature))
+
+	// Field 6 'ForkVersion'
+	hh.PutBytes([]byte(d.ForkVersion))
+
+	// Field 7 'NetworkName'
+	hh.PutBytes([]byte(d.NetworkName))
+
+	hh.Merkleize(idx)
+
+	return nil
 }
