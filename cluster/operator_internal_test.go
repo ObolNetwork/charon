c87cd194c6917aadd58bdb6783f7b4bae16d38cd
xenowits <kumar.abhishek111b@gmail.com>
2022-05-27 14:00:08 +0530

    add deterministic names

--- cluster/operator_internal_test.go
+++ cluster/operator_internal_test.go
@@ -6,13 +6,20 @@ import (
 	"testing"
 )
 
-func TestRandomName(t *testing.T) {
-	first := randomName()
+func TestGetName(t *testing.T) {
+	enr := "enr:-JG4QHBtkNsAMjMNpNpJS4flt2sfkpVoAtLAZXufe1R-vFZ8JSOkuWKyjqZMUuZhp8x0ye6b_j2vV2H_VXr_JPXaUKWGAYEEiHG5gmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQNmdSBrUavbjtQxixlaX-xcy4ci5k7swmRbEHBL-yQNzIN0Y3CCPoODdWRwgj6E"
+	op := Operator{
+		ENR: enr,
+	}
+
+	first, err := op.getName()
+	assert.NoError(t, err)
 	assert.True(t, strings.Contains(first, "-"))
 
-	second := randomName()
+	second, err := op.getName()
+	assert.NoError(t, err)
 	assert.True(t, strings.Contains(second, "-"))
 
-	// Two random names must be different
+	// The two names must be the same.
 	assert.NotEqual(t, first, second)
 }
