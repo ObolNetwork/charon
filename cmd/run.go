44f7dd93efc8c2925ae21b216d094d87d83ef63c
xenowits <kumar.abhishek111b@gmail.com>
2022-06-22 13:45:34 +0530

    interchange ports for 3600 and 3610

--- cmd/run.go
+++ cmd/run.go
@@ -56,7 +56,7 @@ func newRunCmd(runFunc func(context.Context, app.Config) error) *cobra.Command {
 func bindRunFlags(flags *pflag.FlagSet, config *app.Config) {
 	flags.StringVar(&config.LockFile, "lock-file", ".charon/cluster-lock.json", "The path to the cluster lock file defining distributed validator cluster")
 	flags.StringVar(&config.BeaconNodeAddr, "beacon-node-endpoint", "http://localhost/", "Beacon node endpoint URL")
-	flags.StringVar(&config.ValidatorAPIAddr, "validator-api-address", "127.0.0.1:3610", "Listening address (ip and port) for validator-facing traffic proxying the beacon-node API")
+	flags.StringVar(&config.ValidatorAPIAddr, "validator-api-address", "127.0.0.1:3600", "Listening address (ip and port) for validator-facing traffic proxying the beacon-node API")
 	flags.StringVar(&config.MonitoringAddr, "monitoring-address", "127.0.0.1:3620", "Listening address (ip and port) for the monitoring API (prometheus, pprof)")
 	flags.StringVar(&config.JaegerAddr, "jaeger-address", "", "Listening address for jaeger tracing")
 	flags.StringVar(&config.JaegerService, "jaeger-service", "charon", "Service name used for jaeger tracing")
@@ -80,7 +80,7 @@ func bindP2PFlags(flags *pflag.FlagSet, config *p2p.Config) {
 	flags.StringVar(&config.UDPAddr, "p2p-udp-address", "127.0.0.1:3630", "Listening UDP address (ip and port) for discv5 discovery.")
 	flags.StringVar(&config.ExternalIP, "p2p-external-ip", "", "The IP address advertised by libp2p. This may be used to advertise an external IP.")
 	flags.StringVar(&config.ExternalHost, "p2p-external-hostname", "", "The DNS hostname advertised by libp2p. This may be used to advertise an external DNS.")
-	flags.StringSliceVar(&config.TCPAddrs, "p2p-tcp-address", []string{"127.0.0.1:3600"}, "Comma-separated list of listening TCP addresses (ip and port) for libP2P traffic.")
+	flags.StringSliceVar(&config.TCPAddrs, "p2p-tcp-address", []string{"127.0.0.1:3610"}, "Comma-separated list of listening TCP addresses (ip and port) for libP2P traffic.")
 	flags.StringVar(&config.Allowlist, "p2p-allowlist", "", "Comma-separated list of CIDR subnets for allowing only certain peer connections. Example: 192.168.0.0/16 would permit connections to peers on your local network only. The default is to accept all connections.")
 	flags.StringVar(&config.Denylist, "p2p-denylist", "", "Comma-separated list of CIDR subnets for disallowing certain peer connections. Example: 192.168.0.0/16 would disallow connections to peers on your local network. The default is to accept all connections.")
 }
--- testutil/compose/run.go
+++ testutil/compose/run.go
@@ -83,7 +83,7 @@ func getVC(typ VCType, nodeIdx int, numVals int) (vc, error) {
 			Command: `|
       validator-client
       --network=auto
-      --beacon-node-api-endpoint="http://node{{.NodeIdx}}:3610"
+      --beacon-node-api-endpoint="http://node{{.NodeIdx}}:3600"
       {{range .TekuKeys}}--validator-keys="{{.}}"
       {{end -}}
       --validators-proposer-default-fee-recipient="0x0000000000000000000000000000000000000000"`,
