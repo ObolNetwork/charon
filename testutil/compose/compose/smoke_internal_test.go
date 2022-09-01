85ff5f76eb568ac70e1f00a7ce9e94304e2bd09a
Dhruv Bodani <dhruvbodani2510@gmail.com>
2022-09-01 23:20:13 +0530

    dkg: fix dkg and smoke test (#1071)
    Fixes deadlock in dkg and reduce NumValidators in "very large" smoke test which was causing timeout in our github actions.
    
    category: bug
    ticket: none

--- testutil/compose/compose/smoke_internal_test.go
+++ testutil/compose/compose/smoke_internal_test.go
@@ -82,7 +82,7 @@ func TestSmoke(t *testing.T) {
 			ConfigFunc: func(conf *compose.Config) {
 				conf.NumNodes = 21
 				conf.Threshold = 14
-				conf.NumValidators = 1000
+				conf.NumValidators = 100 // TODO(dhruv): Increase to 1000 once we have faster way to create keystores
 				conf.KeyGen = compose.KeyGenCreate
 			},
 		},
