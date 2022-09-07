dd3eb572f664ea1faa763b4b08ba761cabab570d
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2022-09-07 19:39:39 +0530

    core/bcast: fix linter (#1089)
    Fix linter error by adding `gocognit` nolint directive to `Broadcast()` method.
    
    category: fixbuild
    ticket: none

--- core/bcast/bcast.go
+++ core/bcast/bcast.go
@@ -52,9 +52,7 @@ type Broadcaster struct {
 }
 
 // Broadcast broadcasts the aggregated signed duty data object to the beacon-node.
-func (b Broadcaster) Broadcast(ctx context.Context, duty core.Duty,
-	pubkey core.PubKey, aggData core.SignedData,
-) (err error) { //nolint:nonamedreturn // Handled in defer
+func (b Broadcaster) Broadcast(ctx context.Context, duty core.Duty, pubkey core.PubKey, aggData core.SignedData) (err error) { //nolint:nonamedreturn,gocognit // Handled in defer
 	ctx = log.WithTopic(ctx, "bcast")
 	ctx = log.WithCtx(ctx, z.Any("pubkey", pubkey))
 	defer func() {
