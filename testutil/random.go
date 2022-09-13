5b5c4e7bb14cdead2acde4d4c32fded69e2bd71c
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2022-09-13 19:11:39 +0530

    core/bcast: add support for `DutyAggregator` (#1123)
    Add support for `DutyAggregator` to bcast.
    
    category: feature
    ticket: #1096

--- testutil/random.go
+++ testutil/random.go
@@ -360,6 +360,21 @@ func RandomSignedBeaconCommitteeSubscription(vIdx, slot, commIdx int) core.Signe
 	}
 }
 
+func RandomSignedAggregateAndProof() *eth2p0.SignedAggregateAndProof {
+	return &eth2p0.SignedAggregateAndProof{
+		Message:   RandomAggregateAndProof(),
+		Signature: RandomEth2Signature(),
+	}
+}
+
+func RandomAggregateAndProof() *eth2p0.AggregateAndProof {
+	return &eth2p0.AggregateAndProof{
+		AggregatorIndex: RandomVIdx(),
+		Aggregate:       RandomAttestation(),
+		SelectionProof:  RandomEth2Signature(),
+	}
+}
+
 func RandomSyncAggregate(t *testing.T) *altair.SyncAggregate {
 	t.Helper()
 
