024f5ced95e2dc1b8665de8daf22fcf17f913425
corver <corver@obol.tech>
2022-10-25 11:17:49 +0200

    Update p2p/relay.go
    Co-authored-by: Abhishek Kumar <43061995+xenowits@users.noreply.github.com>

--- p2p/relay.go
+++ p2p/relay.go
@@ -92,7 +92,7 @@ func NewRelayReserver(tcpNode host.Host, relay *MutablePeer) lifecycle.HookFunc 
 			// When the reservation expires, the server needs to re-reserve.
 			// When the connection expires (stream reset error), then client needs to reconnect.
 
-			refreshDelay := time.Until(resv.Expiration.Add(-time.Minute * 2))
+			refreshDelay := time.Until(resv.Expiration.Add(-2 * time.Minute))
 
 			log.Debug(ctx, "Relay circuit reserved",
 				z.Any("reservation_expire", resv.Expiration),        // Server side reservation expiry (long)
