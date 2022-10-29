0857c0b7a393c5f6b331a46ae9ee3690cd5327ad
Dhruv Bodani <dhruvbodani2510@gmail.com>
2022-10-29 19:33:51 +0530

    cleanup

--- core/types.go
+++ core/types.go
@@ -33,7 +33,7 @@ import (
 type DutyType int
 
 const (
-	// DomainName enums MUST not change, it will break backwards compatibility.
+	// DutyType enums MUST not change, it will break backwards compatibility.
 
 	DutyUnknown                 DutyType = 0
 	DutyProposer                DutyType = 1
@@ -385,7 +385,7 @@ type SignedData interface {
 
 type Eth2SignedData interface {
 	SignedData
-	// DutyType returns Duty type associated with underlying signed data.
+	// DomainName returns domain associated with underlying signed data.
 	DomainName() signing.DomainName
 	// Epoch returns eth2p0.Epoch associated with underlying type.
 	Epoch(ctx context.Context, eth2Cl eth2wrap.Client) (eth2p0.Epoch, error)
