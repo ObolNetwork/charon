40cca289b17afc2e2710d49ddb1206dee38459f5
corver <corver@obol.tech>
2022-12-01 07:10:37 +0200

    Apply suggestions from code review
    Co-authored-by: Abhishek Kumar <43061995+xenowits@users.noreply.github.com>

--- app/featureset/featureset.go
+++ app/featureset/featureset.go
@@ -46,10 +46,10 @@ const (
 	MockAlpha Feature = "mock_alpha"
 
 	// RelayDiscovery enables relay peer discovery and disables discv5:
-	//   - If a direct connection to a peer is not possible then try to connect to it via all the provided bootnodes/relays
+	//   - If a direct connection to a peer is not possible then try to connect to it via all the provided bootnodes/relays.
 	//   - Direct connections are either not possible since no addresses are known or the addresses do not work.
 	//   - When connected via relay, libp2p's identify protocol detects the remote peer's addresses.
-	//   - Those are added to the peer store so will try to use them.
+	//   - Those are added to the peer store so libp2p will try to use them.
 	RelayDiscovery Feature = "relay_discovery"
 )
 
