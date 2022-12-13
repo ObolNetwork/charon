6a0523d01e9721b16afd5147f461bab526afb678
corverroos <corver.roos@gmail.com>
2022-12-13 14:12:23 +0200

    wip

--- app/qbftdebug.go
+++ app/qbftdebug.go
@@ -29,7 +29,7 @@ import (
 	pbv1 "github.com/obolnetwork/charon/core/corepb/v1"
 )
 
-const maxQBFTDebugger = 2000 * (1 << 20) // 2GB
+const maxQBFTDebugger = 2000 * (1 << 20) // 2GB // TODO(corver): revert to under 50MB.
 
 // newQBFTDebugger returns a new qbftDebugger.
 func newQBFTDebugger() *qbftDebugger {
