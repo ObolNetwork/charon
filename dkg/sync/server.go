711b7d1783fdd76b58222d254f16e046bf8a6bd8
Dhruv Bodani <dhruvbodani2510@gmail.com>
2022-12-13 19:49:49 +0530

    add peer disconnected log to server

--- dkg/sync/server.go
+++ dkg/sync/server.go
@@ -230,7 +230,9 @@ func (s *Server) Start(ctx context.Context) {
 
 		ctx = log.WithCtx(ctx, z.Str("peer", p2p.PeerName(stream.Conn().RemotePeer())))
 		err := s.handleStream(ctx, stream)
-		if err != nil {
+		if isRelayError(err) {
+			log.Warn(ctx, "Peer disconnected", nil, z.Str("peer", p2p.PeerName(stream.Conn().RemotePeer())))
+		} else if err != nil {
 			log.Warn(ctx, "Error serving sync protocol", err)
 		}
 	})
