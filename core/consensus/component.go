df62aff4bd9fa303e7afd5fa60d639df6f071a78
Dhruv Bodani <dhruvbodani2510@gmail.com>
2022-12-13 14:59:27 +0530

    core/consensus: add histogram metric for consensus duration (#1526)
    Adds a histogram metric to report consensus duration for each consensus instance.
    
    category: feature
    ticket: #1247

--- core/consensus/component.go
+++ core/consensus/component.go
@@ -75,8 +75,6 @@ func newDefinition(nodes int, subs func() []subscriber) qbft.Definition[core.Dut
 				return
 			}
 
-			decidedRoundsGauge.WithLabelValues(duty.Type.String()).Set(float64(qcommit[0].Round()))
-
 			for _, sub := range subs() {
 				if err := sub(ctx, duty, value); err != nil {
 					log.Warn(ctx, "Subscriber error", err)
@@ -301,10 +299,28 @@ func (c *Component) propose(ctx context.Context, duty core.Duty, value proto.Mes
 		Receive:   t.recvBuffer,
 	}
 
+	// Instrument consensus instance.
+	var (
+		t0      = time.Now()
+		def     = c.def
+		decided bool
+	)
+	// Wrap Decide function of c.def to instrument consensus instance with provided start time (t0) and decided round.
+	def.Decide = func(ctx context.Context, duty core.Duty, val [32]byte, qcommit []qbft.Msg[core.Duty, [32]byte]) {
+		decided = true
+		instrumentConsensus(duty, qcommit[0].Round(), t0)
+		c.def.Decide(ctx, duty, val, qcommit)
+	}
+
 	// Run the algo, blocking until the context is cancelled.
-	err = qbft.Run[core.Duty, [32]byte](ctx, c.def, qt, duty, peerIdx, hash)
+	err = qbft.Run[core.Duty, [32]byte](ctx, def, qt, duty, peerIdx, hash)
 	if err != nil && !isContextErr(err) {
+		consensusError.Inc()
 		return err // Only return non-context errors.
+	}
+
+	if !decided {
+		consensusTimeout.WithLabelValues(duty.Type.String()).Inc()
 	}
 
 	return nil
--- core/consensus/metrics.go
+++ core/consensus/metrics.go
@@ -16,14 +16,46 @@
 package consensus
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/obolnetwork/charon/app/promauto"
+	"github.com/obolnetwork/charon/core"
 )
 
-var decidedRoundsGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Namespace: "core",
-	Subsystem: "consensus",
-	Name:      "decided_rounds",
-	Help:      "Number of rounds it took to decide consensus instances by duty type.",
-}, []string{"duty"}) // Using gauge since the value changes slowly, once per slot.
+var (
+	decidedRoundsGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "core",
+		Subsystem: "consensus",
+		Name:      "decided_rounds",
+		Help:      "Number of rounds it took to decide consensus instances by duty type.",
+	}, []string{"duty"}) // Using gauge since the value changes slowly, once per slot.
+
+	consensusDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "core",
+		Subsystem: "consensus",
+		Name:      "duration_seconds",
+		Help:      "Duration of a consensus instance in seconds by duty",
+		Buckets:   []float64{.05, .1, .25, .5, 1, 2.5, 5, 10, 20, 30, 60},
+	}, []string{"duty"})
+
+	consensusTimeout = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "core",
+		Subsystem: "consensus",
+		Name:      "timeout_total",
+		Help:      "Total count of consensus timeouts by duty",
+	}, []string{"duty"})
+
+	consensusError = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "core",
+		Subsystem: "consensus",
+		Name:      "error_total",
+		Help:      "Total count of consensus errors",
+	})
+)
+
+func instrumentConsensus(duty core.Duty, round int64, startTime time.Time) {
+	decidedRoundsGauge.WithLabelValues(duty.Type.String()).Set(float64(round))
+	consensusDuration.WithLabelValues(duty.Type.String()).Observe(time.Since(startTime).Seconds())
+}
