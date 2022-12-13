20575d7cb340c2d86253c050eff91e17b409a5bc
Luke Hackett <luke@obol.tech>
2022-12-13 15:10:02 +0100

    testutil/promrated: add initial basic looping and read in config (#1548)
    Start of https://github.com/ObolNetwork/charon/issues/1540. Per corvers suggestion taking incremental steps instead of a large code dump. This starts by reading the necessary config values, then runs on a loop every 10 mins with some logging. After this I will expose some metrics to prometheus then start some of the more complex logic for fetching clusters and polling rated.
    
    ticket: https://github.com/ObolNetwork/charon/issues/1540
    
    category: misc

---
+++ testutil/promrated/promrated.go
@@ -0,0 +1,41 @@
+// Copyright © 2022 Obol Labs Inc.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Command promrated grabs rated stats for all monitored charon clusters
+
+package promrated
+
+import (
+	"context"
+	"time"
+
+	"github.com/obolnetwork/charon/app/log"
+	"github.com/obolnetwork/charon/app/z"
+)
+
+type Config struct {
+	RatedAPIEndpoint string
+	PromAuth         string
+	MonitoringAddr   string
+}
+
+func Run(ctx context.Context, config Config) error {
+	for {
+		log.Info(ctx, "Promrated looping.", z.Str("endpoint", config.RatedAPIEndpoint))
+
+		sleepFor := time.Minute * time.Duration(10)
+		time.Sleep(sleepFor)
+	}
+}
