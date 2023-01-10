ObolNetwork/charon

commit f1892ec6bd9a7bc02fa045b64a6698c55130ae0a

core/scheduler: delete expired duties (#1596)
Delete expired duties from scheduler.

category: feature
ticket: #1584

app/app.go
@@ -404,7 +404,7 @@ func wireCoreWorkflow(ctx context.Context, life *lifecycle.Manager, conf Config,
 		return core.NewDeadliner(ctx, label, deadlineFunc)
 	}
 
-	sched, err := scheduler.New(corePubkeys, eth2Cl, conf.BuilderAPI)
+	sched, err := scheduler.New(corePubkeys, eth2Cl, deadlinerFunc("scheduler"), conf.BuilderAPI)
 	if err != nil {
 		return err
 	}

core/scheduler/scheduler.go
@@ -39,11 +39,11 @@ type delayFunc func(duty core.Duty, deadline time.Time) <-chan time.Time
 
 // NewForT returns a new scheduler for testing using a fake clock.
 func NewForT(t *testing.T, clock clockwork.Clock, delayFunc delayFunc, pubkeys []core.PubKey,
-	eth2Cl eth2wrap.Client, builderAPI bool,
+	eth2Cl eth2wrap.Client, deadliner core.Deadliner, builderAPI bool,
 ) *Scheduler {
 	t.Helper()
 
-	s, err := New(pubkeys, eth2Cl, builderAPI)
+	s, err := New(pubkeys, eth2Cl, deadliner, builderAPI)
 	require.NoError(t, err)
 
 	s.clock = clock
@@ -53,7 +53,7 @@ func NewForT(t *testing.T, clock clockwork.Clock, delayFunc delayFunc, pubkeys [
 }
 
 // New returns a new scheduler.
-func New(pubkeys []core.PubKey, eth2Cl eth2wrap.Client, builderAPI bool) (*Scheduler, error) {
+func New(pubkeys []core.PubKey, eth2Cl eth2wrap.Client, deadliner core.Deadliner, builderAPI bool) (*Scheduler, error) {
 	return &Scheduler{
 		eth2Cl:  eth2Cl,
 		pubkeys: pubkeys,
@@ -66,6 +66,7 @@ func New(pubkeys []core.PubKey, eth2Cl eth2wrap.Client, builderAPI bool) (*Sched
 		metricSubmitter: newMetricSubmitter(),
 		resolvedEpoch:   math.MaxUint64,
 		builderAPI:      builderAPI,
+		deadliner:       deadliner,
 	}, nil
 }
 
@@ -74,6 +75,7 @@ type Scheduler struct {
 	pubkeys         []core.PubKey
 	quit            chan struct{}
 	clock           clockwork.Clock
+	deadliner       core.Deadliner
 	delayFunc       delayFunc
 	metricSubmitter metricSubmitter
 	resolvedEpoch   uint64
@@ -118,6 +120,8 @@ func (s *Scheduler) Run() error {
 		select {
 		case <-s.quit:
 			return nil
+		case duty := <-s.deadliner.C():
+			s.deleteDuty(duty)
 		case slot := <-slotTicker:
 			log.Debug(ctx, "Slot ticked", z.I64("slot", slot.Slot)) // Not adding slot to context since duty will be added that also contains slot.
 
@@ -254,7 +258,7 @@ func (s *Scheduler) resolveDuties(ctx context.Context, slot core.Slot) error {
 	activeValsGauge.Set(float64(len(vals)))
 
 	if len(vals) == 0 {
-		log.Info(ctx, "No active DVs for slot", z.I64("slot", slot.Slot))
+		log.Info(ctx, "No active validators for slot", z.I64("slot", slot.Slot))
 		return nil
 	}
 
@@ -311,6 +315,11 @@ func (s *Scheduler) resolveAttDuties(ctx context.Context, slot core.Slot, vals v
 			continue
 		}
 
+		if !s.deadliner.Add(duty) {
+			log.Warn(ctx, "Ignoring unexpected expired attester duty", nil, z.I64("slot", slot.Slot))
+			continue
+		}
+
 		if !s.setDutyDefinition(duty, pubkey, core.NewAttesterDefinition(attDuty)) {
 			continue
 		}
@@ -324,6 +333,12 @@ func (s *Scheduler) resolveAttDuties(ctx context.Context, slot core.Slot, vals v
 
 		// Schedule aggregation duty as well.
 		aggDuty := core.NewAggregatorDuty(int64(attDuty.Slot))
+
+		if !s.deadliner.Add(aggDuty) {
+			log.Warn(ctx, "Ignoring unexpected expired aggregation duty", nil, z.I64("slot", slot.Slot))
+			continue
+		}
+
 		if !s.setDutyDefinition(aggDuty, pubkey, core.NewAttesterDefinition(attDuty)) {
 			continue
 		}
@@ -367,6 +382,11 @@ func (s *Scheduler) resolveProDuties(ctx context.Context, slot core.Slot, vals v
 			continue
 		}
 
+		if !s.deadliner.Add(duty) {
+			log.Warn(ctx, "Ignoring unexpected expired proposer duty", nil, z.I64("slot", slot.Slot))
+			continue
+		}
+
 		if !s.setDutyDefinition(duty, pubkey, core.NewProposerDefinition(proDuty)) {
 			continue
 		}
@@ -407,6 +427,12 @@ func (s *Scheduler) resolveSyncCommDuties(ctx context.Context, slot core.Slot, v
 		for sl := startSlot; sl.Epoch() == currEpoch; sl = sl.Next() {
 			// Schedule sync committee contribution aggregation.
 			duty := core.NewSyncContributionDuty(sl.Slot)
+
+			if !s.deadliner.Add(duty) {
+				log.Warn(ctx, "Ignoring unexpected expired sync contribution duty", nil, z.I64("slot", slot.Slot))
+				continue
+			}
+
 			s.setDutyDefinition(duty, pubkey, core.NewSyncCommitteeDefinition(syncCommDuty))
 		}
 
@@ -429,6 +455,7 @@ func (s *Scheduler) getDutyDefinitionSet(duty core.Duty) (core.DutyDefinitionSet
 	return defSet, ok
 }
 
+// setDutyDefinition returns true if the duty definition for the pubkey was set, false if it was already set.
 func (s *Scheduler) setDutyDefinition(duty core.Duty, pubkey core.PubKey, set core.DutyDefinition) bool {
 	s.dutiesMutex.Lock()
 	defer s.dutiesMutex.Unlock()
@@ -448,9 +475,6 @@ func (s *Scheduler) setDutyDefinition(duty core.Duty, pubkey core.PubKey, set co
 }
 
 // deleteDuty deletes the duty from the cache.
-// TODO(corver): Call this on duty deadline to trim duties.
-//
-//nolint:unused
 func (s *Scheduler) deleteDuty(duty core.Duty) {
 	s.dutiesMutex.Lock()
 	defer s.dutiesMutex.Unlock()

core/scheduler/scheduler_test.go
@@ -67,7 +67,7 @@ func TestIntegration(t *testing.T) {
 		"0xb790b322e1cce41c48e3c344cf8d752bdc3cfd51e8eeef44a4bdaac081bc92b53b73e823a9878b5d7a532eb9d9dce1e3",
 	}
 
-	s, err := scheduler.New(pubkeys, eth2Cl, false)
+	s, err := scheduler.New(pubkeys, eth2Cl, new(testDeadliner), false)
 	require.NoError(t, err)
 
 	count := 10
@@ -154,7 +154,7 @@ func TestSchedulerWait(t *testing.T) {
 				}, err
 			}
 
-			sched := scheduler.NewForT(t, clock, new(delayer).delay, nil, eth2Cl, false)
+			sched := scheduler.NewForT(t, clock, new(delayer).delay, nil, eth2Cl, new(testDeadliner), false)
 			sched.Stop() // Just run wait functions, then quit.
 			require.NoError(t, sched.Run())
 			require.EqualValues(t, test.WaitSecs, clock.Since(t0).Seconds())
@@ -225,7 +225,7 @@ func TestSchedulerDuties(t *testing.T) {
 			// Construct scheduler
 			clock := newTestClock(t0)
 			delayer := new(delayer)
-			sched := scheduler.NewForT(t, clock, delayer.delay, pubkeys, eth2Cl, false)
+			sched := scheduler.NewForT(t, clock, delayer.delay, pubkeys, eth2Cl, new(testDeadliner), false)
 
 			// Only test scheduler output for first N slots, so Stop scheduler (and slotTicker) after that.
 			const stopAfter = 3
@@ -297,9 +297,10 @@ func TestSchedulerDuties(t *testing.T) {
 
 func TestScheduler_GetDuty(t *testing.T) {
 	var (
-		t0     time.Time
-		slot   = int64(1)
-		valSet = beaconmock.ValidatorSetA
+		t0        time.Time
+		slot      = int64(1)
+		valSet    = beaconmock.ValidatorSetA
+		deadliner = newTestDeadliner()
 	)
 
 	// Configure beacon mock.
@@ -318,7 +319,7 @@ func TestScheduler_GetDuty(t *testing.T) {
 
 	// Construct scheduler.
 	clock := newTestClock(t0)
-	sched := scheduler.NewForT(t, clock, new(delayer).delay, pubkeys, eth2Cl, false)
+	sched := scheduler.NewForT(t, clock, new(delayer).delay, pubkeys, eth2Cl, deadliner, false)
 
 	_, err = sched.GetDutyDefinition(context.Background(), core.NewAttesterDuty(slot))
 	require.ErrorContains(t, err, "epoch not resolved yet")
@@ -357,6 +358,19 @@ func TestScheduler_GetDuty(t *testing.T) {
 		for _, pubKey := range pubKeys {
 			require.NotNil(t, res[pubKey])
 		}
+
+		// Expire all duties
+
+		deadliner.Expire()
+
+		_, err = sched.GetDutyDefinition(context.Background(), core.NewAttesterDuty(slot))
+		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
+
+		_, err = sched.GetDutyDefinition(context.Background(), core.NewAggregatorDuty(slot))
+		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
+
+		_, err = sched.GetDutyDefinition(context.Background(), core.NewSyncContributionDuty(slot))
+		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
 
 		sched.Stop()
 	})
@@ -448,4 +462,43 @@ func (c *testClock) NewTicker(time.Duration) clockwork.Ticker {
 
 func (c *testClock) NewTimer(time.Duration) clockwork.Timer {
 	panic("not supported")
+}
+
+func newTestDeadliner() *testDeadliner {
+	return &testDeadliner{
+		ch: make(chan core.Duty),
+	}
+}
+
+// testDeadliner is a mock deadliner implementation.
+type testDeadliner struct {
+	mu    sync.Mutex
+	added []core.Duty
+	ch    chan core.Duty
+}
+
+func (d *testDeadliner) Add(duty core.Duty) bool {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.added = append(d.added, duty)
+
+	return true
+}
+
+func (d *testDeadliner) C() <-chan core.Duty {
+	return d.ch
+}
+
+func (d *testDeadliner) Expire() {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for _, duty := range d.added {
+		d.ch <- duty
+	}
+
+	d.ch <- core.Duty{} // Ensure all duties are processed before returning.
+
+	d.added = nil
 }

commit 68f27b93535bd6d2d802458615c9c213c999b05d

core/scheduler: trim duties after 3 epochs (#1616)
Fixes issue introduced by #1596 that trimmed data after 5 slots which caused inclusion delay calculation to stop working (it requires duties of 32 ago). Instead just delete duties after 3 epochs (avoid races and off-by-one issues of 2 epochs).

category: bug
ticket: none

app/app.go
@@ -406,7 +406,7 @@ func wireCoreWorkflow(ctx context.Context, life *lifecycle.Manager, conf Config,
 		return core.NewDeadliner(ctx, label, deadlineFunc)
 	}
 
-	sched, err := scheduler.New(corePubkeys, eth2Cl, deadlinerFunc("scheduler"), conf.BuilderAPI)
+	sched, err := scheduler.New(corePubkeys, eth2Cl, conf.BuilderAPI)
 	if err != nil {
 		return err
 	}

core/scheduler/scheduler.go
@@ -34,16 +34,18 @@ import (
 	"github.com/obolnetwork/charon/core"
 )
 
+const trimEpochOffset = 3 // Trim cached duties after 3 epochs. Note inclusion delay calculation requires now-32 slot duties.
+
 // delayFunc abstracts slot offset delaying/sleeping for deterministic tests.
 type delayFunc func(duty core.Duty, deadline time.Time) <-chan time.Time
 
 // NewForT returns a new scheduler for testing using a fake clock.
 func NewForT(t *testing.T, clock clockwork.Clock, delayFunc delayFunc, pubkeys []core.PubKey,
-	eth2Cl eth2wrap.Client, deadliner core.Deadliner, builderAPI bool,
+	eth2Cl eth2wrap.Client, builderAPI bool,
 ) *Scheduler {
 	t.Helper()
 
-	s, err := New(pubkeys, eth2Cl, deadliner, builderAPI)
+	s, err := New(pubkeys, eth2Cl, builderAPI)
 	require.NoError(t, err)
 
 	s.clock = clock
@@ -53,20 +55,20 @@ func NewForT(t *testing.T, clock clockwork.Clock, delayFunc delayFunc, pubkeys [
 }
 
 // New returns a new scheduler.
-func New(pubkeys []core.PubKey, eth2Cl eth2wrap.Client, deadliner core.Deadliner, builderAPI bool) (*Scheduler, error) {
+func New(pubkeys []core.PubKey, eth2Cl eth2wrap.Client, builderAPI bool) (*Scheduler, error) {
 	return &Scheduler{
-		eth2Cl:  eth2Cl,
-		pubkeys: pubkeys,
-		quit:    make(chan struct{}),
-		duties:  make(map[core.Duty]core.DutyDefinitionSet),
-		clock:   clockwork.NewRealClock(),
+		eth2Cl:        eth2Cl,
+		pubkeys:       pubkeys,
+		quit:          make(chan struct{}),
+		duties:        make(map[core.Duty]core.DutyDefinitionSet),
+		dutiesByEpoch: make(map[int64][]core.Duty),
+		clock:         clockwork.NewRealClock(),
 		delayFunc: func(_ core.Duty, deadline time.Time) <-chan time.Time {
 			return time.After(time.Until(deadline))
 		},
 		metricSubmitter: newMetricSubmitter(),
-		resolvedEpoch:   math.MaxUint64,
+		resolvedEpoch:   math.MaxInt64,
 		builderAPI:      builderAPI,
-		deadliner:       deadliner,
 	}, nil
 }
 
@@ -75,11 +77,11 @@ type Scheduler struct {
 	pubkeys         []core.PubKey
 	quit            chan struct{}
 	clock           clockwork.Clock
-	deadliner       core.Deadliner
 	delayFunc       delayFunc
 	metricSubmitter metricSubmitter
-	resolvedEpoch   uint64
+	resolvedEpoch   int64
 	duties          map[core.Duty]core.DutyDefinitionSet
+	dutiesByEpoch   map[int64][]core.Duty
 	dutiesMutex     sync.Mutex
 	dutySubs        []func(context.Context, core.Duty, core.DutyDefinitionSet) error
 	slotSubs        []func(context.Context, core.Slot) error
@@ -120,8 +122,6 @@ func (s *Scheduler) Run() error {
 		select {
 		case <-s.quit:
 			return nil
-		case duty := <-s.deadliner.C():
-			s.deleteDuty(duty)
 		case slot := <-slotTicker:
 			log.Debug(ctx, "Slot ticked", z.I64("slot", slot.Slot)) // Not adding slot to context since duty will be added that also contains slot.
 
@@ -160,15 +160,18 @@ func (s *Scheduler) GetDutyDefinition(ctx context.Context, duty core.Duty) (core
 		return nil, err
 	}
 
-	epoch := uint64(duty.Slot) / slotsPerEpoch
+	epoch := duty.Slot / int64(slotsPerEpoch)
 	if !s.isEpochResolved(epoch) {
 		return nil, errors.New("epoch not resolved yet")
+	}
+	if s.isEpochTrimmed(epoch) {
+		return nil, errors.New("epoch already trimmed")
 	}
 
 	defSet, ok := s.getDutyDefinitionSet(duty)
 	if !ok {
 		return nil, errors.Wrap(core.ErrNotFound, "duty not present for resolved epoch",
-			z.Any("duty", duty), z.U64("epoch", epoch))
+			z.Any("duty", duty), z.I64("epoch", epoch))
 	}
 
 	return defSet.Clone() // Clone before returning.
@@ -176,7 +179,7 @@ func (s *Scheduler) GetDutyDefinition(ctx context.Context, duty core.Duty) (core
 
 // scheduleSlot resolves upcoming duties and triggers resolved duties for the slot.
 func (s *Scheduler) scheduleSlot(ctx context.Context, slot core.Slot) {
-	if s.getResolvedEpoch() != uint64(slot.Epoch()) {
+	if s.getResolvedEpoch() != slot.Epoch() {
 		err := s.resolveDuties(ctx, slot)
 		if err != nil {
 			log.Warn(ctx, "Resolving duties error (retrying next slot)", err, z.I64("slot", slot.Slot))
@@ -277,7 +280,8 @@ func (s *Scheduler) resolveDuties(ctx context.Context, slot core.Slot) error {
 		return err
 	}
 
-	s.setResolvedEpoch(uint64(slot.Epoch()))
+	s.setResolvedEpoch(slot.Epoch())
+	s.trimDuties(slot.Epoch() - trimEpochOffset)
 
 	return nil
 }
@@ -315,12 +319,7 @@ func (s *Scheduler) resolveAttDuties(ctx context.Context, slot core.Slot, vals v
 			continue
 		}
 
-		if !s.deadliner.Add(duty) {
-			log.Warn(ctx, "Ignoring unexpected expired attester duty", nil, z.I64("slot", slot.Slot))
-			continue
-		}
-
-		if !s.setDutyDefinition(duty, pubkey, core.NewAttesterDefinition(attDuty)) {
+		if !s.setDutyDefinition(duty, slot.Epoch(), pubkey, core.NewAttesterDefinition(attDuty)) {
 			continue
 		}
 
@@ -334,12 +333,7 @@ func (s *Scheduler) resolveAttDuties(ctx context.Context, slot core.Slot, vals v
 		// Schedule aggregation duty as well.
 		aggDuty := core.NewAggregatorDuty(int64(attDuty.Slot))
 
-		if !s.deadliner.Add(aggDuty) {
-			log.Warn(ctx, "Ignoring unexpected expired aggregation duty", nil, z.I64("slot", slot.Slot))
-			continue
-		}
-
-		if !s.setDutyDefinition(aggDuty, pubkey, core.NewAttesterDefinition(attDuty)) {
+		if !s.setDutyDefinition(aggDuty, slot.Epoch(), pubkey, core.NewAttesterDefinition(attDuty)) {
 			continue
 		}
 	}
@@ -382,12 +376,7 @@ func (s *Scheduler) resolveProDuties(ctx context.Context, slot core.Slot, vals v
 			continue
 		}
 
-		if !s.deadliner.Add(duty) {
-			log.Warn(ctx, "Ignoring unexpected expired proposer duty", nil, z.I64("slot", slot.Slot))
-			continue
-		}
-
-		if !s.setDutyDefinition(duty, pubkey, core.NewProposerDefinition(proDuty)) {
+		if !s.setDutyDefinition(duty, slot.Epoch(), pubkey, core.NewProposerDefinition(proDuty)) {
 			continue
 		}
 
@@ -428,12 +417,7 @@ func (s *Scheduler) resolveSyncCommDuties(ctx context.Context, slot core.Slot, v
 			// Schedule sync committee contribution aggregation.
 			duty := core.NewSyncContributionDuty(sl.Slot)
 
-			if !s.deadliner.Add(duty) {
-				log.Warn(ctx, "Ignoring unexpected expired sync contribution duty", nil, z.I64("slot", slot.Slot))
-				continue
-			}
-
-			s.setDutyDefinition(duty, pubkey, core.NewSyncCommitteeDefinition(syncCommDuty))
+			s.setDutyDefinition(duty, slot.Epoch(), pubkey, core.NewSyncCommitteeDefinition(syncCommDuty))
 		}
 
 		log.Info(ctx, "Resolved sync committee duty",
@@ -456,7 +440,7 @@ func (s *Scheduler) getDutyDefinitionSet(duty core.Duty) (core.DutyDefinitionSet
 }
 
 // setDutyDefinition returns true if the duty definition for the pubkey was set, false if it was already set.
-func (s *Scheduler) setDutyDefinition(duty core.Duty, pubkey core.PubKey, set core.DutyDefinition) bool {
+func (s *Scheduler) setDutyDefinition(duty core.Duty, epoch int64, pubkey core.PubKey, set core.DutyDefinition) bool {
 	s.dutiesMutex.Lock()
 	defer s.dutiesMutex.Unlock()
 
@@ -470,38 +454,58 @@ func (s *Scheduler) setDutyDefinition(duty core.Duty, pubkey core.PubKey, set co
 
 	defSet[pubkey] = set
 	s.duties[duty] = defSet
+	s.dutiesByEpoch[epoch] = append(s.dutiesByEpoch[epoch], duty)
 
 	return true
 }
 
-// deleteDuty deletes the duty from the cache.
-func (s *Scheduler) deleteDuty(duty core.Duty) {
-	s.dutiesMutex.Lock()
-	defer s.dutiesMutex.Unlock()
-
-	delete(s.duties, duty)
-}
-
-func (s *Scheduler) getResolvedEpoch() uint64 {
+func (s *Scheduler) getResolvedEpoch() int64 {
 	s.dutiesMutex.Lock()
 	defer s.dutiesMutex.Unlock()
 
 	return s.resolvedEpoch
 }
 
-func (s *Scheduler) setResolvedEpoch(epoch uint64) {
+func (s *Scheduler) setResolvedEpoch(epoch int64) {
 	s.dutiesMutex.Lock()
 	defer s.dutiesMutex.Unlock()
 
 	s.resolvedEpoch = epoch
 }
 
-func (s *Scheduler) isEpochResolved(epoch uint64) bool {
-	if s.getResolvedEpoch() == math.MaxUint64 {
+// isEpochResolved returns true if the.
+func (s *Scheduler) isEpochResolved(epoch int64) bool {
+	if s.getResolvedEpoch() == math.MaxInt64 {
 		return false
 	}
 
 	return s.getResolvedEpoch() >= epoch
+}
+
+// isEpochTrimmed returns true if the epoch's duties have been trimmed.
+func (s *Scheduler) isEpochTrimmed(epoch int64) bool {
+	if s.getResolvedEpoch() == math.MaxInt64 {
+		return false
+	}
+
+	return s.getResolvedEpoch() >= epoch+trimEpochOffset
+}
+
+// trimDuties deletes all duties for the provided epoch.
+func (s *Scheduler) trimDuties(epoch int64) {
+	s.dutiesMutex.Lock()
+	defer s.dutiesMutex.Unlock()
+
+	duties := s.dutiesByEpoch[epoch]
+	if len(duties) == 0 {
+		return
+	}
+
+	for _, duty := range duties {
+		delete(s.duties, duty)
+	}
+
+	delete(s.dutiesByEpoch, epoch)
 }
 
 // newSlotTicker returns a blocking channel that will be populated with new slots in real time.

app/monitoringapi_internal_test.go
@@ -144,12 +144,12 @@ func TestStartChecker(t *testing.T) {
 			if tt.err != nil {
 				require.Eventually(t, func() bool {
 					err = readyErrFunc()
-					if !errors.Is(err, errReadyUninitialised) {
-						require.EqualError(t, err, tt.err.Error())
-						return true
+					if !errors.Is(err, tt.err) {
+						t.Logf("Ignoring unexpected error, got=%v, want=%v", err, tt.err)
+						return false
 					}
 
-					return false
+					return true
 				}, time.Second, 100*time.Millisecond)
 			} else {
 				require.Eventually(t, func() bool {

core/scheduler/scheduler_test.go
@@ -67,7 +67,7 @@ func TestIntegration(t *testing.T) {
 		"0xb790b322e1cce41c48e3c344cf8d752bdc3cfd51e8eeef44a4bdaac081bc92b53b73e823a9878b5d7a532eb9d9dce1e3",
 	}
 
-	s, err := scheduler.New(pubkeys, eth2Cl, new(testDeadliner), false)
+	s, err := scheduler.New(pubkeys, eth2Cl, false)
 	require.NoError(t, err)
 
 	count := 10
@@ -154,7 +154,7 @@ func TestSchedulerWait(t *testing.T) {
 				}, err
 			}
 
-			sched := scheduler.NewForT(t, clock, new(delayer).delay, nil, eth2Cl, new(testDeadliner), false)
+			sched := scheduler.NewForT(t, clock, new(delayer).delay, nil, eth2Cl, false)
 			sched.Stop() // Just run wait functions, then quit.
 			require.NoError(t, sched.Run())
 			require.EqualValues(t, test.WaitSecs, clock.Since(t0).Seconds())
@@ -225,7 +225,7 @@ func TestSchedulerDuties(t *testing.T) {
 			// Construct scheduler
 			clock := newTestClock(t0)
 			delayer := new(delayer)
-			sched := scheduler.NewForT(t, clock, delayer.delay, pubkeys, eth2Cl, new(testDeadliner), false)
+			sched := scheduler.NewForT(t, clock, delayer.delay, pubkeys, eth2Cl, false)
 
 			// Only test scheduler output for first N slots, so Stop scheduler (and slotTicker) after that.
 			const stopAfter = 3
@@ -297,10 +297,10 @@ func TestSchedulerDuties(t *testing.T) {
 
 func TestScheduler_GetDuty(t *testing.T) {
 	var (
-		t0        time.Time
-		slot      = int64(1)
-		valSet    = beaconmock.ValidatorSetA
-		deadliner = newTestDeadliner()
+		ctx    = context.Background()
+		t0     time.Time
+		slot   = int64(1)
+		valSet = beaconmock.ValidatorSetA
 	)
 
 	// Configure beacon mock.
@@ -319,25 +319,25 @@ func TestScheduler_GetDuty(t *testing.T) {
 
 	// Construct scheduler.
 	clock := newTestClock(t0)
-	sched := scheduler.NewForT(t, clock, new(delayer).delay, pubkeys, eth2Cl, deadliner, false)
+	sched := scheduler.NewForT(t, clock, new(delayer).delay, pubkeys, eth2Cl, false)
 
-	_, err = sched.GetDutyDefinition(context.Background(), core.NewAttesterDuty(slot))
+	_, err = sched.GetDutyDefinition(ctx, core.NewAttesterDuty(slot))
 	require.ErrorContains(t, err, "epoch not resolved yet")
 
-	_, err = sched.GetDutyDefinition(context.Background(), core.NewAggregatorDuty(slot))
+	_, err = sched.GetDutyDefinition(ctx, core.NewAggregatorDuty(slot))
 	require.ErrorContains(t, err, "epoch not resolved yet")
 
-	_, err = sched.GetDutyDefinition(context.Background(), core.NewSyncContributionDuty(slot))
+	_, err = sched.GetDutyDefinition(ctx, core.NewSyncContributionDuty(slot))
 	require.ErrorContains(t, err, "epoch not resolved yet")
 
-	_, err = sched.GetDutyDefinition(context.Background(), core.NewBuilderProposerDuty(slot))
+	_, err = sched.GetDutyDefinition(ctx, core.NewBuilderProposerDuty(slot))
 	require.ErrorContains(t, err, "builder-api not enabled")
 
-	slotDuration, err := eth2Cl.SlotDuration(context.Background())
+	slotDuration, err := eth2Cl.SlotDuration(ctx)
 	require.NoError(t, err)
 
 	clock.CallbackAfter(t0.Add(slotDuration).Add(time.Second), func() {
-		res, err := sched.GetDutyDefinition(context.Background(), core.NewAttesterDuty(slot))
+		res, err := sched.GetDutyDefinition(ctx, core.NewAttesterDuty(slot))
 		require.NoError(t, err)
 
 		pubKeys, err := valSet.CorePubKeys()
@@ -347,30 +347,34 @@ func TestScheduler_GetDuty(t *testing.T) {
 			require.NotNil(t, res[pubKey])
 		}
 
-		res, err = sched.GetDutyDefinition(context.Background(), core.NewAggregatorDuty(slot))
+		res, err = sched.GetDutyDefinition(ctx, core.NewAggregatorDuty(slot))
 		require.NoError(t, err)
 		for _, pubKey := range pubKeys {
 			require.NotNil(t, res[pubKey])
 		}
 
-		res, err = sched.GetDutyDefinition(context.Background(), core.NewSyncContributionDuty(slot))
+		res, err = sched.GetDutyDefinition(ctx, core.NewSyncContributionDuty(slot))
 		require.NoError(t, err)
 		for _, pubKey := range pubKeys {
 			require.NotNil(t, res[pubKey])
 		}
+	})
 
-		// Expire all duties
+	slotsPerEpoch, err := eth2Cl.SlotsPerEpoch(ctx)
+	require.NoError(t, err)
 
-		deadliner.Expire()
+	// Expire all duties
+	const trimEpochOffset = 3
+	expiry := time.Duration(trimEpochOffset*slotsPerEpoch) * slotDuration
+	clock.CallbackAfter(t0.Add(expiry).Add(time.Second), func() {
+		_, err = sched.GetDutyDefinition(ctx, core.NewAttesterDuty(slot))
+		require.ErrorContains(t, err, "epoch already trimmed")
 
-		_, err = sched.GetDutyDefinition(context.Background(), core.NewAttesterDuty(slot))
-		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
+		_, err = sched.GetDutyDefinition(ctx, core.NewAggregatorDuty(slot))
+		require.ErrorContains(t, err, "epoch already trimmed")
 
-		_, err = sched.GetDutyDefinition(context.Background(), core.NewAggregatorDuty(slot))
-		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
-
-		_, err = sched.GetDutyDefinition(context.Background(), core.NewSyncContributionDuty(slot))
-		require.ErrorContains(t, err, "duty not present for resolved epoch: not found")
+		_, err = sched.GetDutyDefinition(ctx, core.NewSyncContributionDuty(slot))
+		require.ErrorContains(t, err, "epoch already trimmed")
 
 		sched.Stop()
 	})
@@ -409,7 +413,8 @@ func (d *delayer) delay(duty core.Duty, deadline time.Time) <-chan time.Time {
 
 func newTestClock(now time.Time) *testClock {
 	return &testClock{
-		now: now,
+		now:       now,
+		callbacks: make(map[time.Time]func()),
 	}
 }
 
@@ -417,17 +422,15 @@ func newTestClock(now time.Time) *testClock {
 // that is advanced by calls to Sleep or After.
 // Note this *does not* support concurrency.
 type testClock struct {
-	now           time.Time
-	callbackAfter time.Time
-	callback      func()
+	now       time.Time
+	callbacks map[time.Time]func()
 }
 
 // CallbackAfter sets a callback function that is called once
 // before Sleep returns at or after the time has been reached.
 // It is useful to trigger logic "when a certain time has been reached".
 func (c *testClock) CallbackAfter(after time.Time, callback func()) {
-	c.callbackAfter = after
-	c.callback = callback
+	c.callbacks[after] = callback
 }
 
 func (c *testClock) After(d time.Duration) <-chan time.Time {
@@ -441,11 +444,14 @@ func (c *testClock) After(d time.Duration) <-chan time.Time {
 
 func (c *testClock) Sleep(d time.Duration) {
 	c.now = c.now.Add(d)
-	if c.callback == nil || c.now.Before(c.callbackAfter) {
-		return
+
+	for after, callback := range c.callbacks {
+		if c.now.Before(after) {
+			continue
+		}
+		callback()
+		delete(c.callbacks, after)
 	}
-	c.callback()
-	c.callback = nil
 }
 
 func (c *testClock) Now() time.Time {
@@ -462,43 +468,4 @@ func (c *testClock) NewTicker(time.Duration) clockwork.Ticker {
 
 func (c *testClock) NewTimer(time.Duration) clockwork.Timer {
 	panic("not supported")
-}
-
-func newTestDeadliner() *testDeadliner {
-	return &testDeadliner{
-		ch: make(chan core.Duty),
-	}
-}
-
-// testDeadliner is a mock deadliner implementation.
-type testDeadliner struct {
-	mu    sync.Mutex
-	added []core.Duty
-	ch    chan core.Duty
-}
-
-func (d *testDeadliner) Add(duty core.Duty) bool {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
-	d.added = append(d.added, duty)
-
-	return true
-}
-
-func (d *testDeadliner) C() <-chan core.Duty {
-	return d.ch
-}
-
-func (d *testDeadliner) Expire() {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
-	for _, duty := range d.added {
-		d.ch <- duty
-	}
-
-	d.ch <- core.Duty{} // Ensure all duties are processed before returning.
-
-	d.added = nil
 }