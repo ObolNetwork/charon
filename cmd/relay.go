3bd546733b625b0de59cb88dd80bb58ce4fc041f
xenowits <kumar.abhishek111b@gmail.com>
2023-01-16 21:27:33 +0530

    add endpoint to relay command description

--- cmd/relay.go
+++ cmd/relay.go
@@ -32,7 +32,7 @@ func newRelayCmd(runFunc func(context.Context, relay.Config) error) *cobra.Comma
 	cmd := &cobra.Command{
 		Use:   "relay",
 		Short: "Start a libp2p relay server",
-		Long:  "Starts a libp2p relay that charon nodes can use to bootstrap their p2p cluster",
+		Long:  "Starts a libp2p relay that charon nodes can use to bootstrap their p2p cluster.",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := log.InitLogger(config.LogConfig); err != nil {
