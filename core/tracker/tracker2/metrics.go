392d3305bdd9c67beb32b93b89fb67bd6fa23da4
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-01-24 10:45:31 +0530

    update metrics names to tracker2 to avoid duplication

--- core/tracker/tracker2/metrics.go
+++ core/tracker/tracker2/metrics.go
@@ -24,35 +24,35 @@ import (
 var (
 	participationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "core",
-		Subsystem: "tracker",
+		Subsystem: "tracker2",
 		Name:      "participation",
 		Help:      "Set to 1 if peer participated successfully for the given duty or else 0",
 	}, []string{"duty", "peer"})
 
 	participationCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "core",
-		Subsystem: "tracker",
+		Subsystem: "tracker2",
 		Name:      "participation_total",
 		Help:      "Total number of successful participations by peer and duty type",
 	}, []string{"duty", "peer"})
 
 	failedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "core",
-		Subsystem: "tracker",
+		Subsystem: "tracker2",
 		Name:      "failed_duties_total",
 		Help:      "Total number of failed duties by type",
 	}, []string{"duty"})
 
 	unexpectedEventsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "core",
-		Subsystem: "tracker",
+		Subsystem: "tracker2",
 		Name:      "unexpected_events_total",
 		Help:      "Total number of unexpected events by peer",
 	}, []string{"peer"})
 
 	inconsistentCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "core",
-		Subsystem: "tracker",
+		Subsystem: "tracker2",
 		Name:      "inconsistent_parsigs_total",
 		Help:      "Total number of duties that contained inconsistent partial signed data by duty type",
 	}, []string{"duty"})
