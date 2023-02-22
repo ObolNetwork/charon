371431fc4949a5449701ced6741e134ac9ae34de
Gianguido Sorà <gsora@users.noreply.github.com>
2023-02-22 14:23:04 +0100

    cmd: rename combine keystores-dir to cluster-dir

--- cmd/combine.go
+++ cmd/combine.go
@@ -24,10 +24,10 @@ import (
 	"github.com/obolnetwork/charon/combine"
 )
 
-func newCombineCmd(runFunc func(ctx context.Context, keystoresDir string, force bool) error) *cobra.Command {
+func newCombineCmd(runFunc func(ctx context.Context, clusterDir string, force bool) error) *cobra.Command {
 	var (
-		keystoresDir string
-		force        bool
+		clusterDir string
+		force      bool
 	)
 
 	cmd := &cobra.Command{
@@ -36,24 +36,24 @@ func newCombineCmd(runFunc func(ctx context.Context, keystoresDir string, force 
 		Long:  "Combines the private key shares from a threshold of operators in a distributed validator cluster into a set of validator private keys that can be imported into a standard Ethereum validator client.\n\nWarning: running the resulting private keys in a validator alongside the original distributed validator cluster *will* result in slashing.",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runFunc(cmd.Context(), keystoresDir, force)
+			return runFunc(cmd.Context(), clusterDir, force)
 		},
 	}
 
 	bindCombineFlags(
 		cmd.Flags(),
-		&keystoresDir,
+		&clusterDir,
 		&force,
 	)
 
 	return cmd
 }
 
-func newCombineFunc(ctx context.Context, keystoresDir string, force bool) error {
-	return combine.Combine(ctx, keystoresDir, force)
+func newCombineFunc(ctx context.Context, clusterDir string, force bool) error {
+	return combine.Combine(ctx, clusterDir, force)
 }
 
-func bindCombineFlags(flags *pflag.FlagSet, keystoresDir *string, force *bool) {
-	flags.StringVar(keystoresDir, "keystores-dir", "./", `Directory containing all the keystore files organized by ENR, and the lock file.`)
+func bindCombineFlags(flags *pflag.FlagSet, clusterDir *string, force *bool) {
+	flags.StringVar(clusterDir, "cluster-dir", ".charon/cluster/", `Parent directory containing a number of .charon subdirectories from each node in the cluster.`)
 	flags.BoolVar(force, "force", false, "Overwrites private keys with the same name if present.")
 }
