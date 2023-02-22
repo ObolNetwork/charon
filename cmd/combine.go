d21717a6cef642011b63857a67c6fb0f760285b5
Gianguido Sorà <gsora@users.noreply.github.com>
2023-02-22 14:18:33 +0100

    cmd: reword combine command
    Co-authored-by: Oisín Kyne <4981644+OisinKyne@users.noreply.github.com>

--- cmd/combine.go
+++ cmd/combine.go
@@ -33,7 +33,7 @@ func newCombineCmd(runFunc func(ctx context.Context, keystoresDir string, force 
 	cmd := &cobra.Command{
 		Use:   "combine",
 		Short: "Combines the private key shares of a distributed validator cluster into a set of standard validator private keys.",
-		Long:  "Combines private key shares into a single private key for a distributed validator.\nWarning: running the resulting private key in a validator alongside the original distributed validator will result in slashing.",
+		Long:  "Combines the private key shares from a threshold of operators in a distributed validator cluster into a set of validator private keys that can be imported into a standard Ethereum validator client.\n\nWarning: running the resulting private keys in a validator alongside the original distributed validator cluster *will* result in slashing.",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runFunc(cmd.Context(), keystoresDir, force)
