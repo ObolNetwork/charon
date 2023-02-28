aa5a85b3dc01f1d041b1de15830640e6f7c216a7
Oisín Kyne <oisin@obol.tech>
2023-02-28 12:40:15 +0000

    Remove GPL comment, update xenowits/go-pre-commit-hooks to v0.0.4

--- app/errors/errors.go
+++ app/errors/errors.go
@@ -1,17 +1,4 @@
-// Copyright © 2022 Obol Labs Inc.
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright © 2022-2023 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
 
 // Package errors provides errors with structured fields and stack traces.
 // It is a drop-in replacement for stdlib errors and should be used as such throughout the app.
