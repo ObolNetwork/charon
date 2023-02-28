2e0f0a20766c640cf25871babe236b020b5a4648
Oisín Kyne <4981644+OisinKyne@users.noreply.github.com>
2023-02-28 15:58:36 +0000

    *: switch to BSL preamble (#1839)
    Removes the GPL preamble to go source code, replacing it with BSL preamble.
    
    category: misc
    ticket: #1753

--- core/qbft/qbft.go
+++ core/qbft/qbft.go
@@ -1,17 +1,4 @@
-// Copyright © 2022 Obol Labs Inc.
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright © 2022-2023 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
 
 // Package qbft is an implementation of the https://arxiv.org/pdf/2002.03613.pdf paper
 // referenced by the QBFT spec https://github.com/ConsenSys/qbft-formal-spec-and-verification.
