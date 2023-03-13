c3fe208b5813b62a39325eda29a1561161d37124
corver <corver@obol.tech>
2023-03-13 18:29:22 +0200

    app/version: bump to v0.14.1 (#1877)
    Bump version to v0.14.1.
    
    category: misc
    ticket: none

--- app/version/version.go
+++ app/version/version.go
@@ -13,7 +13,7 @@ import (
 const (
 	// Version is the release version of the codebase.
 	// Usually overridden by tag names when building binaries.
-	Version = "v0.14.0"
+	Version = "v0.14.1"
 )
 
 // Supported returns the supported versions in order of precedence.
