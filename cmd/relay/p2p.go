1766b30a2a1c80515aff52ff94b465a98c25da39
Luke Hackett <luke@obol.tech>
2023-03-15 18:50:21 +0000

    Update p2p.go

--- cmd/relay/p2p.go
+++ cmd/relay/p2p.go
@@ -44,6 +44,7 @@ func startP2P(ctx context.Context, config Config, key *k1.PrivateKey, reporter m
 	limiter := rcmgr.DefaultLimits
 	limiter.SystemBaseLimit.ConnsInbound = config.MaxConns
 	limiter.SystemBaseLimit.Conns = config.MaxConns
+	limiter.SystemBaseLimit.StreamsInbound = config.MaxConns
 	limiter.SystemBaseLimit.StreamsOutbound = config.MaxConns
 	limiter.SystemBaseLimit.FD = config.MaxConns
 	limiter.TransientBaseLimit = limiter.SystemBaseLimit
