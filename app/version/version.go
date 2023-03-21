57af5c7355cd4dc9df8515d21e4563af31e1b622
corverroos <corver.roos@gmail.com>
2023-03-21 08:34:45 +0200

    bump version

--- app/version/version.go
+++ app/version/version.go
@@ -13,7 +13,7 @@ import (
 const (
 	// Version is the release version of the codebase.
 	// Usually overridden by tag names when building binaries.
-	Version = "v0.14.1"
+	Version = "v0.14.3"
 )
 
 // Supported returns the supported versions in order of precedence.
