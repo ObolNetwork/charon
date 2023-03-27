3f261ca9c15ba1654d249238f83dc178be4d2617
corverroos <corver.roos@gmail.com>
2023-03-27 09:22:46 +0200

    cluster: release v1.6 of cluster definition

--- cluster/version.go
+++ cluster/version.go
@@ -5,11 +5,11 @@ package cluster
 import "testing"
 
 const (
-	currentVersion = v1_5
+	currentVersion = v1_6
 	dkgAlgo        = "default"
 
-	v1_6 = "v1.6.0" // Draft
-	v1_5 = "v1.5.0" // Default
+	v1_6 = "v1.6.0" // Default
+	v1_5 = "v1.5.0"
 	v1_4 = "v1.4.0"
 	v1_3 = "v1.3.0"
 	v1_2 = "v1.2.0"
