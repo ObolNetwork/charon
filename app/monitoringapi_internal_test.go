c75ca50858fb103c88f86904bf3e308a9774257f
Gianguido Sorà <gsora@users.noreply.github.com>
2023-03-28 10:10:19 +0200

    app: various race condition fixes (#1949)
    Also fix a race condition in `core/tracker` since it came up in `app` testing.
    
    category: bug
    ticket: #1867

--- app/monitoringapi_internal_test.go
+++ app/monitoringapi_internal_test.go
@@ -79,6 +79,7 @@ func TestStartChecker(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
