ac38cce693fd57ad3d322c1febe73163fae94db7
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2023-04-10 12:23:33 +0530

    eth2util: update deposit cli version (#2074)
    Updates `deposit_cli_version` to `2.5.0`.
    
    category: misc
    ticket: https://github.com/ObolNetwork/charon/issues/2067

--- eth2util/deposit/deposit.go
+++ eth2util/deposit/deposit.go
@@ -28,7 +28,7 @@ var (
 	// DOMAIN_DEPOSIT. See spec: https://benjaminion.xyz/eth2-annotated-spec/phase0/beacon-chain/#domain-types
 	depositDomainType = eth2p0.DomainType([4]byte{0x03, 0x00, 0x00, 0x00})
 
-	depositCliVersion = "2.3.0"
+	depositCliVersion = "2.5.0"
 )
 
 // NewMessage returns a deposit message created using the provided parameters.
