d8c3e1cb6f934ac1ed59c23b5fdefb1669ed3fdf
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2023-04-12 14:15:29 +0530

    rlp: fix bug in decodeLength (#2081)
    Fixes bug in RLP `decodeLength()` by ensuring that calculations of length should use units of bytes instead of bits.
    
    category: bug 
    ticket: #2076

---
+++ eth2util/rlp/rlp_internal_test.go
@@ -0,0 +1,30 @@
+// Copyright © 2022-2023 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
+
+package rlp
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodeLength(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		items := []byte{0xb8, 0x01, 0x0a}
+		offset, length, err := decodeLength(items)
+		require.NoError(t, err)
+		require.Equal(t, 2, offset)
+		require.Equal(t, 1, length)
+	})
+
+	t.Run("prefix absent", func(t *testing.T) {
+		_, _, err := decodeLength(nil)
+		require.ErrorContains(t, err, "input too short")
+	})
+
+	t.Run("prefix provided but length absent", func(t *testing.T) {
+		items := []byte{0xbf}
+		_, _, err := decodeLength(items)
+		require.ErrorContains(t, err, "input too short")
+	})
+}
