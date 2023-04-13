002e8ce80c38a1cab06caac397ad8531e32b3aeb
Gianguido Sorà <gsora@users.noreply.github.com>
2023-04-13 11:57:19 +0200

    app/featureset: remove `herumi_bls` feature flag

--- app/featureset/featureset.go
+++ app/featureset/featureset.go
@@ -38,9 +38,6 @@ const (
 	//   - When connected via relay, libp2p's identify protocol detects the remote peer's addresses.
 	//   - Those are added to the peer store so libp2p will try to use them.
 	RelayDiscovery Feature = "relay_discovery"
-
-	// HerumiBLS enables usage of the Herumi BLS12-381 implementation, rather than Kryptology.
-	HerumiBLS Feature = "herumi_bls"
 )
 
 var (
@@ -50,7 +47,6 @@ var (
 		Priority:       statusStable,
 		MockAlpha:      statusAlpha,
 		RelayDiscovery: statusStable,
-		HerumiBLS:      statusStable,
 		// Add all features and there status here.
 	}
 
