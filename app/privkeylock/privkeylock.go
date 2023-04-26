c6c717f1cc940e44acdcbc0fe75ef0e52e2e94f1
Gianguido Sorà <gsora@users.noreply.github.com>
2023-04-26 10:31:45 +0200

    app/privkeylock: if the lockfile was written more than 5s ago, overwrite it and continue with no error

--- app/privkeylock/privkeylock.go
+++ app/privkeylock/privkeylock.go
@@ -3,45 +3,101 @@
 package privkeylock
 
 import (
+	"encoding/json"
 	"os"
+	"time"
 
 	"github.com/obolnetwork/charon/app/errors"
 	"github.com/obolnetwork/charon/app/z"
 )
 
-// New creates a private key lock file in path, writing contextStr in it.
+var lockfileGracePeriod = func() time.Duration {
+	return 5 * time.Second
+}
+
+type privkeyLockCtx struct {
+	ContextStr string
+	Timestamp  time.Time
+}
+
+func newLockCtx(contextStr string) privkeyLockCtx {
+	return privkeyLockCtx{
+		ContextStr: contextStr,
+		Timestamp:  time.Now(),
+	}
+}
+
+// New creates a private key lock file in path, writing ContextStr in it.
 // If a private key lock file exists at path New returns an error, a clean-up function otherwise.
 func New(path, contextStr string) (func() error, error) {
 	if _, err := os.Stat(path); err == nil {
-		readCtxStr, err := os.ReadFile(path)
+		roCtxFile, err := os.Open(path)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not read private key lock file content even if present")
 		}
 
-		return nil, errors.New(
-			"existing private key lock file found, another charon instance may be running on your machine, if not then you can delete that file",
-			z.Str("path", path),
-			z.Str("context", string(readCtxStr)),
-		)
+		lctx := privkeyLockCtx{}
+		if err := json.NewDecoder(roCtxFile).Decode(&lctx); err != nil {
+			return nil, errors.Wrap(err, "cannot decode private key lock file content")
+		}
+
+		if time.Since(lctx.Timestamp) <= lockfileGracePeriod() {
+			return nil, errors.New(
+				"existing private key lock file found, another charon instance may be running on your machine, if not then you can delete that file",
+				z.Str("path", path),
+				z.Str("context", lctx.ContextStr),
+			)
+		}
+
+		if err := roCtxFile.Close(); err != nil {
+			return nil, errors.Wrap(err,
+				"cannot close private key lock file, manually delete file at path to continue with execution",
+				z.Str("path", path),
+				z.Str("context", lctx.ContextStr),
+			)
+		}
+
+		if err := os.Remove(path); err != nil {
+			return nil, errors.Wrap(err,
+				"could not remove old lock file whose timestamp is past grace period",
+				z.Str("path", path),
+				z.Str("context", lctx.ContextStr),
+			)
+		}
+
+		// safety time has passed, overwrite lockfile with our own data
+		if err := createPrivkeyLock(path, contextStr); err != nil {
+			return nil, errors.Wrap(err,
+				"cannot create private key lock file, manually delete file at path to continue with execution",
+				z.Str("path", path),
+				z.Str("context", lctx.ContextStr),
+			)
+		}
+
+		return defaultCleanupFunc(path), nil
 	} else if errors.Is(err, os.ErrNotExist) {
 		if err := createPrivkeyLock(path, contextStr); err != nil {
 			return nil, err
 		}
 
-		return func() error {
-			if err := os.Remove(path); err != nil {
-				return errors.Wrap(err, "cannot remove private key lock file")
-			}
-
-			return nil
-		}, nil
+		return defaultCleanupFunc(path), nil
 	} else {
 		return nil, errors.Wrap(err, "fatal error while handling private key lock file", z.Str("path", path))
 	}
 }
 
-// createPrivkeyLock creates a file in path with contextStr written inside.
-// It's an overzealous function: if it can't write exactly len(contextStr) bytes in path,
+func defaultCleanupFunc(path string) func() error {
+	return func() error {
+		if err := os.Remove(path); err != nil {
+			return errors.Wrap(err, "cannot remove private key lock file")
+		}
+
+		return nil
+	}
+}
+
+// createPrivkeyLock creates a file in path with ContextStr and a timestamp written inside.
+// It's an overzealous function: if it can't write exactly len(ContextStr, timestamp) bytes in path,
 // it returns error.
 func createPrivkeyLock(path, contextStr string) error {
 	f, err := os.Create(path)
@@ -49,16 +105,22 @@ func createPrivkeyLock(path, contextStr string) error {
 		return errors.Wrap(err, "cannot create private key lock file", z.Str("path", path))
 	}
 
-	amt, err := f.WriteString(contextStr)
+	lctx := newLockCtx(contextStr)
+	content, err := json.Marshal(lctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot marshal private key lock context")
+	}
+
+	amt, err := f.Write(content)
 	if err != nil {
 		return errors.Wrap(err, "cannot write context string in private key lock file", z.Str("path", path))
 	}
 
-	if amt != len(contextStr) {
+	if amt != len(content) {
 		return errors.New(
 			"could not write entirety of context string in private key lock file",
 			z.Str("path", path),
-			z.Int("expected", len(contextStr)),
+			z.Int("expected", len(content)),
 			z.Int("got", amt),
 		)
 	}
--- app/privkeylock/privkeylock_test.go
+++ app/privkeylock/privkeylock_test.go
@@ -1,20 +1,20 @@
 // Copyright © 2022-2023 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
 
-package privkeylock_test
+//nolint:testpackage // needs to overwrite grace period function without exposing it
+package privkeylock
 
 import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/obolnetwork/charon/app/privkeylock"
 )
 
 func TestNewInitsAndDelete(t *testing.T) {
 	temp := t.TempDir()
-	cleanFunc, err := privkeylock.New(filepath.Join(temp, "privkeylocktest"), "test")
+	cleanFunc, err := New(filepath.Join(temp, "privkeylocktest"), "test")
 	require.NoError(t, err)
 	require.NotNil(t, cleanFunc)
 	require.NoError(t, cleanFunc())
@@ -24,13 +24,61 @@ func TestNewInitsAndDelete(t *testing.T) {
 
 func TestNewTwoInitsAndDelete(t *testing.T) {
 	temp := t.TempDir()
-	cleanFunc, err := privkeylock.New(filepath.Join(temp, "privkeylocktest"), "test")
+	cleanFunc, err := New(filepath.Join(temp, "privkeylocktest"), "test")
 	require.NoError(t, err)
 	require.NotNil(t, cleanFunc)
 
-	cleanFunc2, err2 := privkeylock.New(filepath.Join(temp, "privkeylocktest"), "test")
+	cleanFunc2, err2 := New(filepath.Join(temp, "privkeylocktest"), "test")
 	require.ErrorContains(t, err2, "existing private key lock file found, another charon instance may be running on your machine, if not then you can delete that file")
 	require.Nil(t, cleanFunc2)
 
 	require.NoError(t, cleanFunc())
+}
+
+func TestNewAfterGraceWorks(t *testing.T) {
+	oldgrace := lockfileGracePeriod()
+	defer func() {
+		lockfileGracePeriod = func() time.Duration {
+			return oldgrace
+		}
+	}()
+
+	lockfileGracePeriod = func() time.Duration {
+		return 500 * time.Millisecond
+	}
+
+	temp := t.TempDir()
+	cleanFunc, err := New(filepath.Join(temp, "privkeylocktest"), "test")
+	require.NoError(t, err)
+	require.NotNil(t, cleanFunc)
+
+	time.Sleep(500 * time.Millisecond)
+
+	cleanFunc2, err2 := New(filepath.Join(temp, "privkeylocktest"), "test")
+	require.NoError(t, err2)
+	require.NoError(t, cleanFunc2())
+}
+
+func TestNewBeforeGraceDoesntWorks(t *testing.T) {
+	oldgrace := lockfileGracePeriod()
+	defer func() {
+		lockfileGracePeriod = func() time.Duration {
+			return oldgrace
+		}
+	}()
+
+	lockfileGracePeriod = func() time.Duration {
+		return 500 * time.Millisecond
+	}
+
+	temp := t.TempDir()
+	cleanFunc, err := New(filepath.Join(temp, "privkeylocktest"), "test")
+	require.NoError(t, err)
+	require.NotNil(t, cleanFunc)
+
+	time.Sleep(100 * time.Millisecond)
+
+	cleanFunc2, err2 := New(filepath.Join(temp, "privkeylocktest"), "test")
+	require.ErrorContains(t, err2, "existing private key lock file found, another charon instance may be running on your machine, if not then you can delete that file")
+	require.Nil(t, cleanFunc2)
 }
