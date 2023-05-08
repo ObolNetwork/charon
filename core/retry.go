cfb1f2ef7ac496e18e15a16194902a8b37d64316
corverroos <corver.roos@gmail.com>
2023-05-08 11:35:33 +0200

    core: wire async consensus

--- core/retry.go
+++ core/retry.go
@@ -19,6 +19,21 @@ func WithAsyncRetry(retryer *retry.Retryer[Duty]) WireOption {
 
 			return nil
 		}
+		// ConsensusParticipate and ConsensusPropose don't require retrying but they should be called async.
+		w.ConsensusParticipate = func(ctx context.Context, duty Duty) error {
+			go retryer.DoAsync(ctx, duty, "consensus", "participate", func(ctx context.Context) error {
+				return clone.ConsensusParticipate(ctx, duty)
+			})
+
+			return nil
+		}
+		w.ConsensusPropose = func(ctx context.Context, duty Duty, set UnsignedDataSet) error {
+			go retryer.DoAsync(ctx, duty, "consensus", "propose", func(ctx context.Context) error {
+				return clone.ConsensusPropose(ctx, duty, set)
+			})
+
+			return nil
+		}
 		w.ParSigExBroadcast = func(ctx context.Context, duty Duty, set ParSignedDataSet) error {
 			go retryer.DoAsync(ctx, duty, "parsigex", "broadcast", func(ctx context.Context) error {
 				return clone.ParSigExBroadcast(ctx, duty, set)
