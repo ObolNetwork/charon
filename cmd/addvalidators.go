c8e5816ce96204b05f19f72f9d8eb923f114a114
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2023-06-14 17:00:19 +0530

    cmd: add-validators command (#2306)
    Adds logic to `charon alpha add-validators-solo` command using mutable cluster APIs.
    
    category: feature 
    ticket: #1887

--- cmd/addvalidators.go
+++ cmd/addvalidators.go
@@ -3,19 +3,41 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 
+	eth2p0 "github.com/attestantio/go-eth2-client/spec/phase0"
+	k1 "github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/spf13/cobra"
 
 	"github.com/obolnetwork/charon/app/errors"
+	"github.com/obolnetwork/charon/app/k1util"
 	"github.com/obolnetwork/charon/app/z"
+	"github.com/obolnetwork/charon/cluster"
+	"github.com/obolnetwork/charon/cluster/state"
+	"github.com/obolnetwork/charon/eth2util"
+	"github.com/obolnetwork/charon/eth2util/enr"
+	"github.com/obolnetwork/charon/eth2util/registration"
+	"github.com/obolnetwork/charon/tbls"
 )
 
+// addValidatorsConfig is config for the `add-validators` command.
 type addValidatorsConfig struct {
 	Lockfile          string
+	EnrPrivKeyfiles   []string
 	NumVals           int
 	WithdrawalAddrs   []string
 	FeeRecipientAddrs []string
+
+	TestConfig TestConfig
+}
+
+// TestConfig defines additional test-only config.
+type TestConfig struct {
+	// Lock provides the lock explicitly, skips loading from disk.
+	Lock *cluster.Lock
+	// P2PKeys provides the p2p private keys explicitly, skips loading keystores from disk.
+	P2PKeys []*k1.PrivateKey
 }
 
 func newAddValidatorsCmd(runFunc func(context.Context, addValidatorsConfig) error) *cobra.Command {
@@ -36,20 +58,157 @@ func newAddValidatorsCmd(runFunc func(context.Context, addValidatorsConfig) erro
 	return cmd
 }
 
+// bindAddValidatorsFlags binds command line flags for the `add-validators` command.
 func bindAddValidatorsFlags(cmd *cobra.Command, config *addValidatorsConfig) {
 	cmd.Flags().IntVar(&config.NumVals, "num-validators", 1, "The count of new distributed validators to add in the cluster.")
 	cmd.Flags().StringVar(&config.Lockfile, "lock-file", ".charon/cluster-lock.json", "The path to the legacy cluster lock file defining distributed validator cluster.")
+	cmd.Flags().StringSliceVar(&config.EnrPrivKeyfiles, "private-key-files", nil, "Comma separated list of paths to charon enr private key files. This should be in the same order as the operators, ie, first private key file should correspond to the first operator and so on.")
 	cmd.Flags().StringSliceVar(&config.FeeRecipientAddrs, "fee-recipient-addresses", nil, "Comma separated list of Ethereum addresses of the fee recipient for each new validator. Either provide a single fee recipient address or fee recipient addresses for each validator.")
 	cmd.Flags().StringSliceVar(&config.WithdrawalAddrs, "withdrawal-addresses", nil, "Comma separated list of Ethereum addresses to receive the returned stake and accrued rewards for each new validator. Either provide a single withdrawal address or withdrawal addresses for each validator.")
 }
 
 func runAddValidatorsSolo(_ context.Context, conf addValidatorsConfig) (err error) {
-	// TODO(xenowits): Implement this in next PR, see issue https://github.com/ObolNetwork/charon/issues/1887.
-	return validateConf(conf)
+	// Read lock file to load mutable cluster state.
+	cState, err := loadClusterState(conf)
+	if err != nil {
+		return err
+	}
+
+	if err = validateConf(conf, len(cState.Operators)); err != nil {
+		return errors.Wrap(err, "validate config")
+	}
+
+	p2pKeys, err := getP2PKeys(conf)
+	if err != nil {
+		return errors.Wrap(err, "load p2p keys")
+	}
+
+	if err := validateP2PKeysOrder(p2pKeys, cState.Operators); err != nil {
+		return err
+	}
+
+	// If a single address is provided, use the same address for all the validators.
+	if len(conf.FeeRecipientAddrs) == 1 {
+		conf.FeeRecipientAddrs = repeatAddr(conf.FeeRecipientAddrs[0], conf.NumVals)
+		conf.WithdrawalAddrs = repeatAddr(conf.WithdrawalAddrs[0], conf.NumVals)
+	}
+
+	vals, err := genNewVals(len(cState.Operators), cState.Threshold, cState.ForkVersion, conf)
+	if err != nil {
+		return err
+	}
+
+	// Perform a `gen_validators/v0.0.1` mutation using the newly created validators.
+	genVals, err := state.NewGenValidators(cState.Hash, vals)
+	if err != nil {
+		return errors.Wrap(err, "generate validators")
+	}
+
+	genValsHash, err := genVals.Hash()
+	if err != nil {
+		return errors.Wrap(err, "hash gen vals")
+	}
+
+	var approvals []state.SignedMutation
+	for _, p2pKey := range p2pKeys {
+		// Perform individual `node_approval/v0.0.1` mutation using each operator's enr private key.
+		approval, err := state.SignNodeApproval(genValsHash, p2pKey)
+		if err != nil {
+			return err
+		}
+
+		approvals = append(approvals, approval)
+	}
+
+	// Perform a `node_approvals/v0.0.1` parallel composite mutation using above approvals.
+	nodeApprovals, err := state.NewNodeApprovalsComposite(approvals)
+	if err != nil {
+		return errors.Wrap(err, "node approvals")
+	}
+
+	// Perform a `add_validators/v0.0.1` linear composite mutation using `gen_validators` and `node_approvals` mutations.
+	addVals, err := state.NewAddValidators(genVals, nodeApprovals)
+	if err != nil {
+		return errors.Wrap(err, "add validators")
+	}
+
+	// Finally, perform a cluster transformation.
+	_, err = addVals.Transform(cState)
+	if err != nil {
+		return errors.Wrap(err, "transform cluster state")
+	}
+
+	// TODO(xenowits): Write new cluster state to disk, see issue https://github.com/ObolNetwork/charon/issues/1887.
+
+	return nil
+}
+
+// builderRegistration returns a builder registration object using the provided inputs.
+func builderRegistration(secret tbls.PrivateKey, pubkey tbls.PublicKey, feeRecipientAddr string, forkVersion []byte) (state.BuilderRegistration, error) {
+	timestamp, err := eth2util.ForkVersionToGenesisTime(forkVersion)
+	if err != nil {
+		return state.BuilderRegistration{}, errors.Wrap(err, "invalid fork version")
+	}
+
+	reg, err := registration.NewMessage(
+		eth2p0.BLSPubKey(pubkey),
+		feeRecipientAddr,
+		registration.DefaultGasLimit,
+		timestamp,
+	)
+	if err != nil {
+		return state.BuilderRegistration{}, errors.Wrap(err, "create registration message")
+	}
+
+	sigRoot, err := registration.GetMessageSigningRoot(reg, eth2p0.Version(forkVersion))
+	if err != nil {
+		return state.BuilderRegistration{}, errors.Wrap(err, "registration signing root")
+	}
+
+	sig, err := tbls.Sign(secret, sigRoot[:])
+	if err != nil {
+		return state.BuilderRegistration{}, errors.Wrap(err, "sign registration root")
+	}
+
+	return state.BuilderRegistration{
+		Message: state.Registration{
+			FeeRecipient: reg.FeeRecipient[:],
+			GasLimit:     int(reg.GasLimit),
+			Timestamp:    timestamp,
+			PubKey:       reg.Pubkey[:],
+		},
+		Signature: sig[:],
+	}, nil
+}
+
+// loadClusterState returns the cluster state from the given file path.
+func loadClusterState(conf addValidatorsConfig) (state.Cluster, error) {
+	if conf.TestConfig.Lock != nil {
+		return state.NewClusterFromLock(*conf.TestConfig.Lock)
+	}
+
+	verifyLock := func(lock cluster.Lock) error {
+		if err := lock.VerifyHashes(); err != nil {
+			return errors.Wrap(err, "cluster lock hash verification failed")
+		}
+
+		if err := lock.VerifySignatures(); err != nil {
+			return errors.Wrap(err, "cluster lock signature verification failed")
+		}
+
+		return nil
+	}
+
+	clusterState, err := state.Load(conf.Lockfile, verifyLock)
+	if err != nil {
+		return state.Cluster{}, errors.Wrap(err, "load cluster state")
+	}
+
+	return clusterState, nil
 }
 
 // validateConf returns an error if the provided validators config fails validation checks.
-func validateConf(conf addValidatorsConfig) error {
+func validateConf(conf addValidatorsConfig, numOps int) error {
 	if conf.NumVals <= 0 {
 		return errors.New("insufficient validator count", z.Int("validators", conf.NumVals))
 	}
@@ -67,6 +226,14 @@ func validateConf(conf addValidatorsConfig) error {
 			z.Int("fee_recipients", len(conf.FeeRecipientAddrs)),
 			z.Int("withdrawal_addresses", len(conf.WithdrawalAddrs)),
 		)
+	}
+
+	privKeysCount := len(conf.EnrPrivKeyfiles)
+	if len(conf.TestConfig.P2PKeys) > 0 {
+		privKeysCount = len(conf.TestConfig.P2PKeys)
+	}
+	if privKeysCount != numOps {
+		return errors.New("insufficient enr private key files", z.Int("num_operators", numOps), z.Int("num_keyfiles", len(conf.EnrPrivKeyfiles)))
 	}
 
 	if conf.NumVals > 1 {
@@ -89,4 +256,118 @@ func validateConf(conf addValidatorsConfig) error {
 	}
 
 	return nil
+}
+
+// genNewVals returns a list of new validators from the provided config.
+func genNewVals(numOps, threshold int, forkVersion []byte, conf addValidatorsConfig) ([]state.Validator, error) {
+	// Generate new validators
+	var vals []state.Validator
+	for i := 0; i < conf.NumVals; i++ {
+		// Generate private/public keypair
+		secret, err := tbls.GenerateSecretKey()
+		if err != nil {
+			return []state.Validator{}, errors.Wrap(err, "generate secret key")
+		}
+
+		pubkey, err := tbls.SecretToPublicKey(secret)
+		if err != nil {
+			return []state.Validator{}, errors.Wrap(err, "generate public key")
+		}
+
+		// Split private key and generate public keyshares
+		shares, err := tbls.ThresholdSplit(secret, uint(numOps), uint(threshold))
+		if err != nil {
+			return []state.Validator{}, errors.Wrap(err, "threshold split key")
+		}
+
+		var pubshares [][]byte
+		for _, share := range shares {
+			pubshare, err := tbls.SecretToPublicKey(share)
+			if err != nil {
+				return []state.Validator{}, errors.Wrap(err, "generate public key")
+			}
+
+			pubshares = append(pubshares, pubshare[:])
+		}
+
+		feeRecipientAddr, err := eth2util.ChecksumAddress(conf.FeeRecipientAddrs[i])
+		if err != nil {
+			return []state.Validator{}, errors.Wrap(err, "invalid fee recipient address")
+		}
+
+		withdrawalAddr, err := eth2util.ChecksumAddress(conf.WithdrawalAddrs[i])
+		if err != nil {
+			return []state.Validator{}, errors.Wrap(err, "invalid withdrawal address")
+		}
+
+		// Generate builder registration
+		builderReg, err := builderRegistration(secret, pubkey, feeRecipientAddr, forkVersion)
+		if err != nil {
+			return []state.Validator{}, err
+		}
+
+		vals = append(vals, state.Validator{
+			PubKey:              pubkey[:],
+			PubShares:           pubshares,
+			FeeRecipientAddress: feeRecipientAddr,
+			WithdrawalAddress:   withdrawalAddr,
+			BuilderRegistration: builderReg,
+		})
+	}
+
+	return vals, nil
+}
+
+// getP2PKeys returns a list of p2p private keys either by loading from disk or from test config.
+func getP2PKeys(conf addValidatorsConfig) ([]*k1.PrivateKey, error) {
+	if len(conf.TestConfig.P2PKeys) > 0 {
+		return conf.TestConfig.P2PKeys, nil
+	}
+
+	var p2pKeys []*k1.PrivateKey
+	for _, enrKeyFile := range conf.EnrPrivKeyfiles {
+		p2pKey, err := k1util.Load(enrKeyFile)
+		if err != nil {
+			return nil, errors.Wrap(err, "load enr private key")
+		}
+
+		p2pKeys = append(p2pKeys, p2pKey)
+	}
+
+	return p2pKeys, nil
+}
+
+// validateP2PKeysOrder ensures that the provided p2p private keys are ordered correctly by peer index.
+func validateP2PKeysOrder(p2pKeys []*k1.PrivateKey, ops []state.Operator) error {
+	var enrs []string
+	for _, enrStr := range ops {
+		enrs = append(enrs, enrStr.ENR)
+	}
+
+	if len(p2pKeys) != len(enrs) {
+		return errors.New("length of p2p keys and enrs don't match", z.Int("p2pkeys", len(p2pKeys)), z.Int("enrs", len(enrs)))
+	}
+
+	for i, enrStr := range enrs {
+		record, err := enr.Parse(enrStr)
+		if err != nil {
+			return err
+		}
+
+		if !bytes.Equal(p2pKeys[i].PubKey().SerializeCompressed(), record.PubKey.SerializeCompressed()) {
+			return errors.New("invalid p2p key order", z.Int("peer_index", i))
+		}
+	}
+
+	return nil
+}
+
+// repeatAddr repeats the same address for all the validators.
+func repeatAddr(addr string, numVals int) []string {
+	var addrs []string
+	for i := 0; i < numVals; i++ {
+		addrs = append(addrs, addr)
+	}
+
+	return addrs
 }
--- cmd/addvalidators_internal_test.go
+++ cmd/addvalidators_internal_test.go
@@ -3,17 +3,27 @@
 package cmd
 
 import (
+	"context"
 	"testing"
 
+	k1 "github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/stretchr/testify/require"
+
+	"github.com/obolnetwork/charon/cluster"
+	"github.com/obolnetwork/charon/cluster/state"
+	"github.com/obolnetwork/charon/testutil"
+)
+
+const (
+	feeRecipientAddr = "0x0000000000000000000000000000000000000000"
+	enrPrivKeyFile   = ".charon/charon-enr-private-key"
 )
 
 func TestValidateConfigAddValidators(t *testing.T) {
-	const feeRecipientAddr = "0x0000000000000000000000000000000000000000"
-
 	tests := []struct {
 		name   string
 		conf   addValidatorsConfig
+		numOps int
 		errMsg string
 	}{
 		{
@@ -50,6 +60,17 @@ func TestValidateConfigAddValidators(t *testing.T) {
 			errMsg: "fee recipient and withdrawal addresses lengths mismatch",
 		},
 		{
+			name: "insufficient enr private key files",
+			conf: addValidatorsConfig{
+				NumVals:           1,
+				WithdrawalAddrs:   []string{feeRecipientAddr},
+				FeeRecipientAddrs: []string{feeRecipientAddr},
+				EnrPrivKeyfiles:   []string{enrPrivKeyFile},
+			},
+			numOps: 2,
+			errMsg: "insufficient enr private key files",
+		},
+		{
 			name: "single addr for all validators",
 			conf: addValidatorsConfig{
 				NumVals:           2,
@@ -78,7 +99,7 @@ func TestValidateConfigAddValidators(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := validateConf(tt.conf)
+			err := validateConf(tt.conf, tt.numOps)
 			if tt.errMsg != "" {
 				require.Equal(t, tt.errMsg, err.Error())
 			} else {
@@ -86,4 +107,74 @@ func TestValidateConfigAddValidators(t *testing.T) {
 			}
 		})
 	}
+}
+
+// TODO(xenowits): Add more extensive tests, this is just a very simple unit test.
+func TestRunAddValidators(t *testing.T) {
+	const n = 3
+	lock, p2pKeys, _ := cluster.NewForT(t, 1, n, n, 0)
+
+	conf := addValidatorsConfig{
+		NumVals:           1,
+		WithdrawalAddrs:   []string{feeRecipientAddr},
+		FeeRecipientAddrs: []string{feeRecipientAddr},
+		TestConfig: TestConfig{
+			Lock:    &lock,
+			P2PKeys: p2pKeys,
+		},
+	}
+
+	err := runAddValidatorsSolo(context.Background(), conf)
+	require.NoError(t, err)
+}
+
+func TestValidateP2PKeysOrder(t *testing.T) {
+	const (
+		seed = 123
+		n    = 4
+	)
+
+	t.Run("correct order", func(t *testing.T) {
+		var (
+			p2pKeys []*k1.PrivateKey
+			ops     []state.Operator
+		)
+
+		for i := 0; i < n; i++ {
+			key, enrStr := testutil.RandomENR(t, seed+i)
+			p2pKeys = append(p2pKeys, key)
+			ops = append(ops, state.Operator{ENR: enrStr.String()})
+		}
+
+		err := validateP2PKeysOrder(p2pKeys, ops)
+		require.NoError(t, err)
+	})
+
+	t.Run("length mismatch", func(t *testing.T) {
+		key, _ := testutil.RandomENR(t, seed)
+		err := validateP2PKeysOrder([]*k1.PrivateKey{key}, nil)
+		require.ErrorContains(t, err, "length of p2p keys and enrs don't match")
+	})
+
+	t.Run("invalid order", func(t *testing.T) {
+		var (
+			p2pKeys []*k1.PrivateKey
+			ops     []state.Operator
+		)
+
+		for i := 0; i < n; i++ {
+			key, enrStr := testutil.RandomENR(t, seed+i)
+			p2pKeys = append(p2pKeys, key)
+			ops = append(ops, state.Operator{ENR: enrStr.String()})
+		}
+
+		// Swap first and last elements of p2p keys list
+		first := p2pKeys[0]
+		last := p2pKeys[n-1]
+		p2pKeys[0] = last
+		p2pKeys[n-1] = first
+
+		err := validateP2PKeysOrder(p2pKeys, ops)
+		require.ErrorContains(t, err, "invalid p2p key order")
+	})
 }
