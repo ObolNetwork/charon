42d13cf60c9af7f73188bc89e48c592255ed4a2e
corverroos <corver.roos@gmail.com>
2023-06-23 11:04:50 -0400

    cleanup

--- eth2util/types_test.go
+++ eth2util/types_test.go
@@ -6,8 +6,10 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"testing"
 
+	eth2p0 "github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/stretchr/testify/require"
 
 	"github.com/obolnetwork/charon/eth2util"
@@ -26,23 +28,27 @@ func TestEpochHashRoot(t *testing.T) {
 }
 
 func TestUnmarshallingSignedEpoch(t *testing.T) {
+	epoch := eth2p0.Epoch(rand.Int())
 	sig := testutil.RandomBytes96()
-	newTmpl := `{"epoch": 1,"signature": "%#x"}`
-	b := []byte(fmt.Sprintf(newTmpl, sig))
+
+	newTmpl := `{"epoch": %d,"signature": "%#x"}`
+	b := []byte(fmt.Sprintf(newTmpl, epoch, sig))
 
 	var e1 eth2util.SignedEpoch
 	err := e1.UnmarshalJSON(b)
 	testutil.RequireNoError(t, err)
 	require.Equal(t, sig, e1.Signature[:])
+	require.Equal(t, epoch, e1.Epoch)
 
 	type legacySig [96]byte
 	sigB, err := json.Marshal(legacySig(sig))
 	require.NoError(t, err)
-	oldTmpl := `{"epoch": 1,"signature": %s}`
-	b = []byte(fmt.Sprintf(oldTmpl, sigB))
+	oldTmpl := `{"epoch": %d,"signature": %s}`
+	b = []byte(fmt.Sprintf(oldTmpl, epoch, sigB))
 
 	var e2 eth2util.SignedEpoch
 	err = e2.UnmarshalJSON(b)
 	testutil.RequireNoError(t, err)
 	require.Equal(t, sig, e2.Signature[:])
+	require.Equal(t, epoch, e2.Epoch)
 }
