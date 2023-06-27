2fef9f091f41050046f659b144273cd4f0fcbfda
Abhishek Kumar <43061995+xenowits@users.noreply.github.com>
2023-06-27 15:06:31 +0530

    core/tracker: fix inclusion test (#2363)
    Fixes failing `inclusion_internal_test` due to non-deterministic ordering of included duties.
    
    Build: https://github.com/ObolNetwork/charon/actions/runs/5380913909/jobs/9764300053?pr=2361
    
    category: fixbuild 
    ticket: none

--- core/tracker/inclusion_internal_test.go
+++ core/tracker/inclusion_internal_test.go
@@ -90,12 +90,16 @@ func TestInclusion(t *testing.T) {
 	// Create some duties
 	att1 := testutil.RandomAttestation()
 	att1Duty := core.NewAttesterDuty(int64(att1.Data.Slot))
+
 	agg2 := testutil.RandomSignedAggregateAndProof()
 	agg2Duty := core.NewAggregatorDuty(int64(agg2.Message.Aggregate.Data.Slot))
+
 	att3 := testutil.RandomAttestation()
 	att3Duty := core.NewAttesterDuty(int64(att3.Data.Slot))
+
 	block4 := testutil.RandomCapellaVersionedSignedBeaconBlock()
 	block4Duty := core.NewProposerDuty(int64(block4.Capella.Message.Slot))
+
 	block5 := testutil.RandomCapellaVersionedSignedBlindedBeaconBlock()
 	block5.Capella.Message.Body.Graffiti = eth2wrap.GetSyntheticGraffiti() // Ignored, not included or missed.
 	block5Duty := core.NewBuilderProposerDuty(int64(block5.Capella.Message.Slot))
@@ -136,7 +140,8 @@ func TestInclusion(t *testing.T) {
 	incl.CheckBlock(context.Background(), block)
 
 	// Assert that the 1st and 2nd duty was included
-	require.Equal(t, []core.Duty{att1Duty, agg2Duty}, included)
+	duties := []core.Duty{att1Duty, agg2Duty}
+	require.ElementsMatch(t, included, duties)
 
 	// Trim the duties
 	incl.Trim(context.Background(), att3Duty.Slot)
