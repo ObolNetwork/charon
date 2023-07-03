aba2bb5f5f0c57c930f7f0b5d9519bf6fdf3c724
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-07-03 20:21:23 +0530

    p2p: add support for multi protocols to Send (#2387)
    Add support for multi protocols to `p2p.Send(...)`.
    
    category: bug
    ticket: #2386

--- p2p/sender_test.go
+++ p2p/sender_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/obolnetwork/charon/testutil"
 )
 
-func TestSendReceive(t *testing.T) {
+func TestSend(t *testing.T) {
 	var (
 		undelimID = protocol.ID("undelimited")
 		delimID   = protocol.ID("delimited")
@@ -98,12 +98,12 @@ func TestSendReceive(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			testSendReceive(t, test.clientBasicProtoID, test.serverBasicProtoID, delimID, test.delimitedClient, test.delimitedServer)
+			testSend(t, test.clientBasicProtoID, test.serverBasicProtoID, delimID, test.delimitedClient, test.delimitedServer)
 		})
 	}
 }
 
-func testSendReceive(t *testing.T, clientBasicProtoID, serverBasicProtoID, delimitedID protocol.ID, delimitedClient, delimitedServer bool) {
+func testSend(t *testing.T, clientBasicProtoID, serverBasicProtoID, delimitedID protocol.ID, delimitedClient, delimitedServer bool) {
 	t.Helper()
 
 	var (
@@ -125,10 +125,12 @@ func testSendReceive(t *testing.T, clientBasicProtoID, serverBasicProtoID, delim
 
 	client.Peerstore().AddAddrs(server.ID(), server.Addrs(), peerstore.PermanentAddrTTL)
 
+	// Catch server errors.
+	serverErrChan := make(chan error)
+
 	// Register the server handler that either:
 	//  - Errors if slot is negative
-	//  - Echos the duty request if slot is even
-	//  - Returns nothing is slot is odd
+	//  - Returns nothing otherwise
 	p2p.RegisterHandler("server", server, serverBasicProtoID,
 		func() proto.Message { return new(pbv1.Duty) },
 		func(ctx context.Context, peerID peer.ID, req proto.Message) (proto.Message, bool, error) {
@@ -138,23 +140,19 @@ func testSendReceive(t *testing.T, clientBasicProtoID, serverBasicProtoID, delim
 			duty, ok := req.(*pbv1.Duty)
 			require.True(t, ok)
 
+			var err error
+			defer func() {
+				serverErrChan <- err
+			}()
+
 			if duty.Slot < 0 {
-				return nil, false, errNegative
-			} else if duty.Slot%2 == 0 {
-				return duty, true, nil
-			} else {
-				return nil, false, nil
+				err = errNegative
 			}
+
+			return nil, false, err
 		},
 		serverOpt...,
 	)
-
-	sendReceive := func(slot int64) (*pbv1.Duty, error) {
-		resp := new(pbv1.Duty)
-		err := p2p.SendReceive(ctx, client, server.ID(), &pbv1.Duty{Slot: slot}, resp, clientBasicProtoID, clientOpt...)
-
-		return resp, err
-	}
 
 	protocolNotSupported := func() bool {
 		// Client supports ONLY delimited protocol while Server supports ONLY non-delimited protocol.
@@ -171,34 +169,21 @@ func testSendReceive(t *testing.T, clientBasicProtoID, serverBasicProtoID, delim
 	}
 
 	if protocolNotSupported() {
-		_, err := sendReceive(100)
+		err := p2p.Send(ctx, client, clientBasicProtoID, server.ID(), &pbv1.Duty{Slot: 100}, clientOpt...)
 		require.ErrorContains(t, err, "protocols not supported")
 
 		return
 	}
 
 	t.Run("server error", func(t *testing.T) {
-		_, err := sendReceive(-1)
-		if delimitedClient && delimitedServer {
-			require.ErrorContains(t, err, "read response: EOF")
-		} else {
-			require.ErrorContains(t, err, "no or zero response received")
-		}
+		err := p2p.Send(ctx, client, clientBasicProtoID, server.ID(), &pbv1.Duty{Slot: -1}, clientOpt...)
+		require.NoError(t, err)
+		require.ErrorContains(t, <-serverErrChan, "negative slot")
 	})
 
 	t.Run("ok", func(t *testing.T) {
-		slot := int64(100)
-		resp, err := sendReceive(slot)
+		err := p2p.Send(ctx, client, clientBasicProtoID, server.ID(), &pbv1.Duty{Slot: 100}, clientOpt...)
 		require.NoError(t, err)
-		require.Equal(t, slot, resp.Slot)
-	})
-
-	t.Run("empty response", func(t *testing.T) {
-		_, err := sendReceive(101)
-		if delimitedClient && delimitedServer {
-			require.ErrorContains(t, err, "read response: EOF")
-		} else {
-			require.ErrorContains(t, err, "no or zero response received")
-		}
+		require.NoError(t, <-serverErrChan)
 	})
 }
