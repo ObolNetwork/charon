cb5fb776eaabe40bfd5e37111a6ad8c9b10bbe7b
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-07-12 16:51:45 +0530

    fix tracker delay to capture inclusion

--- core/tracker/inclusion.go
+++ core/tracker/inclusion.go
@@ -19,8 +19,9 @@ import (
 
 const (
 	// InclCheckLag is the number of slots to lag before checking inclusion.
-	// Half an epoch is good compromise between finality and responsiveness.
-	InclCheckLag = 16
+	// We wait for 4 slots to mitigate against reorgs as it should cover almost 100% of the cases of reorgs.
+	// Reorgs of more than 4 slots are very rare in ethereum PoS.
+	InclCheckLag = 4
 	// InclMissedLag is the number of slots after which we assume the duty was not included and we
 	// delete cached submissions.
 	InclMissedLag = 32
