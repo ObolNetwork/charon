3c88c0ece98887d230f1fcab8cb69d8ebec41cba
Abhishek Kumar <kumar.abhishek111b@gmail.com>
2023-09-29 17:18:44 +0530

    add reference to staking deposit cli

--- eth2util/deposit/deposit.go
+++ eth2util/deposit/deposit.go
@@ -28,6 +28,7 @@ var (
 	// DOMAIN_DEPOSIT. See spec: https://benjaminion.xyz/eth2-annotated-spec/phase0/beacon-chain/#domain-types
 	depositDomainType = eth2p0.DomainType([4]byte{0x03, 0x00, 0x00, 0x00})
 
+	// https://github.com/ethereum/staking-deposit-cli/blob/master/staking_deposit/settings.py#L4
 	depositCliVersion = "2.7.0"
 )
 
