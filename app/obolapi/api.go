6e191ebc689ad054127f0641ed94cdfbd728f345
Abhishek Kumar <kumar.abhishek111b@gmail.com>
2023-11-08 15:57:15 +0530

    update publish lock timeout to 30s

--- app/obolapi/api.go
+++ app/obolapi/api.go
@@ -49,9 +49,9 @@ func (c Client) url() *url.URL {
 	return baseURL
 }
 
-// PublishLock posts the lockfile to obol-api.
+// PublishLock posts the lockfile to obol-api. It has a 30s timeout.
 func (c Client) PublishLock(ctx context.Context, lock cluster.Lock) error {
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
 	addr := c.url()
