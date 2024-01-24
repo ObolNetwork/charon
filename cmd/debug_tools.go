45ffe92e7b7bb0cdf5950c2c7274fefa5e0722ed
Gianguido Sorà <gsora@users.noreply.github.com>
2024-01-24 11:37:15 +0100

    Update cmd/debug_tools.go
    Co-authored-by: Abhishek Kumar <43061995+xenowits@users.noreply.github.com>

--- cmd/debug_tools.go
+++ cmd/debug_tools.go
@@ -9,5 +9,5 @@ import (
 // bindDebugMonitoringFlags binds Prometheus monitoring and debug address CLI flags. The debug address defaults to an empty address.
 func bindDebugMonitoringFlags(cmd *cobra.Command, monitorAddr, debugAddr *string, defaultMonitorAddr string) {
 	cmd.Flags().StringVar(monitorAddr, "monitoring-address", defaultMonitorAddr, "Listening address (ip and port) for the monitoring API (prometheus).")
-	cmd.Flags().StringVar(debugAddr, "debug-address", "", "Listening address (ip and port) for the pprof and QBFT debug API.")
+	cmd.Flags().StringVar(debugAddr, "debug-address", "", "Listening address (ip and port) for the pprof and QBFT debug API. It is not enabled by default.")
 }
