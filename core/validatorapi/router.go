f819e0746bf6946706dfb5c1d8824e9b46a18c81
Dhruv Bodani <dhruvbodani2510@gmail.com>
2024-02-08 21:32:13 +0530

    core/validatorapi: use go-eth2-client versioning (#2871)
    Use go-eth2-client for block version string for blinded blocks.
    
    category: bug
    ticket: none

--- core/validatorapi/router.go
+++ core/validatorapi/router.go
@@ -750,7 +750,7 @@ func proposeBlindedBlock(p eth2client.BlindedProposalProvider) handlerFunc {
 			}
 
 			return proposeBlindedBlockResponseBellatrix{
-				Version: "BELLATRIX",
+				Version: eth2spec.DataVersionBellatrix.String(),
 				Data:    block.Bellatrix,
 			}, resHeaders, nil
 		case eth2spec.DataVersionCapella:
@@ -759,7 +759,7 @@ func proposeBlindedBlock(p eth2client.BlindedProposalProvider) handlerFunc {
 			}
 
 			return proposeBlindedBlockResponseCapella{
-				Version: "CAPELLA",
+				Version: eth2spec.DataVersionCapella.String(),
 				Data:    block.Capella,
 			}, resHeaders, nil
 		case eth2spec.DataVersionDeneb:
@@ -768,7 +768,7 @@ func proposeBlindedBlock(p eth2client.BlindedProposalProvider) handlerFunc {
 			}
 
 			return proposeBlindedBlockResponseDeneb{
-				Version: "DENEB",
+				Version: eth2spec.DataVersionDeneb.String(),
 				Data:    block.Deneb,
 			}, resHeaders, nil
 		default:
