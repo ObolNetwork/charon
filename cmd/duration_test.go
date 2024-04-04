67b6c90c993f35f685b81ebc9775fd92d1a7df9e
Kaloyan Tanev <kalo@obol.tech>
2024-04-04 09:41:55 +0200

    Duration test non internal

--- cmd/duration_test.go
+++ cmd/duration_test.go
@@ -1,48 +1,50 @@
 // Copyright © 2022-2024 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
 
-package cmd
+package cmd_test
 
 import (
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/obolnetwork/charon/cmd"
 )
 
 func TestDurationMarshalJSON(t *testing.T) {
 	tests := []struct {
 		name        string
-		in          Duration
+		in          cmd.Duration
 		expected    []byte
 		expectedErr string
 	}{
 		{
 			name:        "millisecond",
-			in:          Duration{time.Millisecond},
+			in:          cmd.Duration{time.Millisecond},
 			expected:    []byte("\"1ms\""),
 			expectedErr: "",
 		},
 		{
 			name:        "day",
-			in:          Duration{24 * time.Hour},
+			in:          cmd.Duration{24 * time.Hour},
 			expected:    []byte("\"24h0m0s\""),
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds",
-			in:          Duration{1000 * time.Nanosecond},
+			in:          cmd.Duration{1000 * time.Nanosecond},
 			expected:    []byte("\"1µs\""),
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds",
-			in:          Duration{60 * time.Second},
+			in:          cmd.Duration{60 * time.Second},
 			expected:    []byte("\"1m0s\""),
 			expectedErr: "",
 		},
 		{
 			name:        "empty",
-			in:          Duration{},
+			in:          cmd.Duration{},
 			expected:    []byte("\"0s\""),
 			expectedErr: "",
 		},
@@ -64,85 +66,85 @@ func TestDurationUnmarshalJSON(t *testing.T) {
 	tests := []struct {
 		name        string
 		in          []byte
-		expected    Duration
+		expected    cmd.Duration
 		expectedErr string
 	}{
 		{
 			name:        "millisecond",
 			in:          []byte("\"1ms\""),
-			expected:    Duration{time.Millisecond},
+			expected:    cmd.Duration{time.Millisecond},
 			expectedErr: "",
 		},
 		{
 			name:        "day",
 			in:          []byte("\"24h0m0s\""),
-			expected:    Duration{24 * time.Hour},
+			expected:    cmd.Duration{24 * time.Hour},
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds",
 			in:          []byte("\"1µs\""),
-			expected:    Duration{1000 * time.Nanosecond},
+			expected:    cmd.Duration{1000 * time.Nanosecond},
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds",
 			in:          []byte("\"1m0s\""),
-			expected:    Duration{60 * time.Second},
+			expected:    cmd.Duration{60 * time.Second},
 			expectedErr: "",
 		},
 		{
 			name:        "zero",
 			in:          []byte("\"0s\""),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "",
 		},
 		{
 			name:        "millisecond number",
 			in:          []byte("1000000"),
-			expected:    Duration{time.Millisecond},
+			expected:    cmd.Duration{time.Millisecond},
 			expectedErr: "",
 		},
 		{
 			name:        "day number",
 			in:          []byte("86400000000000"),
-			expected:    Duration{24 * time.Hour},
+			expected:    cmd.Duration{24 * time.Hour},
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds number",
 			in:          []byte("1000"),
-			expected:    Duration{1000 * time.Nanosecond},
+			expected:    cmd.Duration{1000 * time.Nanosecond},
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds number",
 			in:          []byte("60000000000"),
-			expected:    Duration{60 * time.Second},
+			expected:    cmd.Duration{60 * time.Second},
 			expectedErr: "",
 		},
 		{
 			name:        "zero number",
 			in:          []byte("0"),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "",
 		},
 		{
 			name:        "text string",
 			in:          []byte("\"second\""),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "parse string time to duration",
 		},
 		{
 			name:        "invalid json",
 			in:          []byte("second"),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "unmarshal json duration",
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			var res Duration
+			var res cmd.Duration
 			err := res.UnmarshalJSON(test.in)
 			if test.expectedErr != "" {
 				require.ErrorContains(t, err, test.expectedErr)
@@ -157,37 +159,37 @@ func TestDurationUnmarshalJSON(t *testing.T) {
 func TestDurationMarshalText(t *testing.T) {
 	tests := []struct {
 		name        string
-		in          Duration
+		in          cmd.Duration
 		expected    []byte
 		expectedErr string
 	}{
 		{
 			name:        "millisecond",
-			in:          Duration{time.Millisecond},
+			in:          cmd.Duration{time.Millisecond},
 			expected:    []byte("1ms"),
 			expectedErr: "",
 		},
 		{
 			name:        "day",
-			in:          Duration{24 * time.Hour},
+			in:          cmd.Duration{24 * time.Hour},
 			expected:    []byte("24h0m0s"),
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds",
-			in:          Duration{1000 * time.Nanosecond},
+			in:          cmd.Duration{1000 * time.Nanosecond},
 			expected:    []byte("1µs"),
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds",
-			in:          Duration{60 * time.Second},
+			in:          cmd.Duration{60 * time.Second},
 			expected:    []byte("1m0s"),
 			expectedErr: "",
 		},
 		{
 			name:        "empty",
-			in:          Duration{},
+			in:          cmd.Duration{},
 			expected:    []byte("0s"),
 			expectedErr: "",
 		},
@@ -209,79 +211,79 @@ func TestDurationUnmarshalText(t *testing.T) {
 	tests := []struct {
 		name        string
 		in          []byte
-		expected    Duration
+		expected    cmd.Duration
 		expectedErr string
 	}{
 		{
 			name:        "millisecond",
 			in:          []byte("1ms"),
-			expected:    Duration{time.Millisecond},
+			expected:    cmd.Duration{time.Millisecond},
 			expectedErr: "",
 		},
 		{
 			name:        "day",
 			in:          []byte("24h0m0s"),
-			expected:    Duration{24 * time.Hour},
+			expected:    cmd.Duration{24 * time.Hour},
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds",
 			in:          []byte("1µs"),
-			expected:    Duration{1000 * time.Nanosecond},
+			expected:    cmd.Duration{1000 * time.Nanosecond},
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds",
 			in:          []byte("1m0s"),
-			expected:    Duration{60 * time.Second},
+			expected:    cmd.Duration{60 * time.Second},
 			expectedErr: "",
 		},
 		{
 			name:        "zero",
 			in:          []byte("0s"),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "",
 		},
 		{
 			name:        "millisecond number",
 			in:          []byte("1000000"),
-			expected:    Duration{time.Millisecond},
+			expected:    cmd.Duration{time.Millisecond},
 			expectedErr: "",
 		},
 		{
 			name:        "day number",
 			in:          []byte("86400000000000"),
-			expected:    Duration{24 * time.Hour},
+			expected:    cmd.Duration{24 * time.Hour},
 			expectedErr: "",
 		},
 		{
 			name:        "1000 nanoseconds number",
 			in:          []byte("1000"),
-			expected:    Duration{1000 * time.Nanosecond},
+			expected:    cmd.Duration{1000 * time.Nanosecond},
 			expectedErr: "",
 		},
 		{
 			name:        "60 seconds number",
 			in:          []byte("60000000000"),
-			expected:    Duration{60 * time.Second},
+			expected:    cmd.Duration{60 * time.Second},
 			expectedErr: "",
 		},
 		{
 			name:        "zero number",
 			in:          []byte("0"),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "",
 		},
 		{
 			name:        "text string",
 			in:          []byte("second"),
-			expected:    Duration{},
+			expected:    cmd.Duration{},
 			expectedErr: "parse string time to duration",
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			var res Duration
+			var res cmd.Duration
 			err := res.UnmarshalText(test.in)
 			if test.expectedErr != "" {
 				require.ErrorContains(t, err, test.expectedErr)
