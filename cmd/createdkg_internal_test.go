bcef88112feb0424c61272cdd41bdd72dd886081
Alexandre Adomnicai <alexandre@adomnicai.me>
2024-08-28 15:53:05 +0200

    Add whitespace

--- cmd/createdkg_internal_test.go
+++ cmd/createdkg_internal_test.go
@@ -58,7 +58,7 @@ func TestCreateDkgInvalid(t *testing.T) {
 					"-JG4QDKNYm_JK-w6NuRcUFKvJAlq2L4CwkECelzyCVrMWji4YnVRn8AqQEL5fTQotPL2MKxiKNmn2k6XEINtq-6O3Z2GAYGvzr_LgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQKlO7fSaBa3h48CdM-qb_Xb2_hSrJOy6nNjR0mapAqMboN0Y3CCDhqDdWRwgg4u",
 				}, validENRs...),
 				Threshold: 3,
-				Network: defaultNetwork,
+				Network:   defaultNetwork,
 			},
 			errMsg: "invalid ENR: missing 'enr:' prefix",
 		},
