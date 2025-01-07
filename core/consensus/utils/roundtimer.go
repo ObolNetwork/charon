19b8a46e798997a90d5f2c467dcbb03f3fa68adc
Diogo Santos <diogo@obol.tech>
2025-01-07 14:09:15 +0000

    add exponential timer to GetTimerFunc()

--- core/consensus/utils/roundtimer.go
+++ core/consensus/utils/roundtimer.go
@@ -25,6 +25,13 @@ type TimerFunc func(core.Duty) RoundTimer
 
 // GetTimerFunc returns a timer function based on the enabled features.
 func GetTimerFunc() TimerFunc {
+
+	if featureset.Enabled(featureset.Exponential) {
+		return func(core.Duty) RoundTimer {
+			return NewExponentialRoundTimer()
+		}
+	}
+
 	if featureset.Enabled(featureset.EagerDoubleLinear) {
 		return func(core.Duty) RoundTimer {
 			return NewDoubleEagerLinearRoundTimer()
@@ -177,6 +184,11 @@ func (t *exponentialRoundTimer) Timer(round int64) (<-chan time.Time, func()) {
 		timer = t.clock.NewTimer(time.Millisecond * time.Duration(math.Pow(2, float64(round-1))*100))
 	}
 	return timer.Chan(), func() { timer.Stop() }
+}
+
+// NewExponentialRoundTimer returns a new exponential round timer type.
+func NewExponentialRoundTimer() RoundTimer {
+	return NewExponentialRoundTimerWithClock(clockwork.NewRealClock())
 }
 
 func NewExponentialRoundTimerWithClock(clock clockwork.Clock) RoundTimer {
