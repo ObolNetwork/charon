bc041bdb95b46ebcf8b46e6e145bbc79917ca5fe
Diogo Santos <diogo@obol.tech>
2025-02-11 14:52:12 +0000

    add flag to create dkg and add lock to eth1 client initialization

--- app/eth1wrap/client.go
+++ app/eth1wrap/client.go
@@ -1,6 +1,8 @@
 package eth1wrap
 
 import (
+	"sync"
+
 	"github.com/obolnetwork/charon/app/errors"
 	erc1271 "github.com/obolnetwork/charon/app/eth1wrap/generated"
 
@@ -12,40 +14,59 @@ var (
 	ERC1271_MAGIC_VALUE = [4]byte{0x16, 0x26, 0xba, 0x7e}
 )
 
-// NewEth1Client returns a initiliazed eth1 JSON-RPC client
-func NewEth1Client(executionEngineAddress string) (*Client, error) {
-	cl := NewLazyEth1Client(executionEngineAddress)
+// NewEth1Client returns a initiliazed eth1 JSON-RPC client.
+func NewEth1Client(addr string) (*Client, error) {
+	cl := NewLazyEth1Client(addr)
 	err := cl.initializeClient()
 
 	return cl, err
 }
 
-// NewEth1Client returns an uninitialized eth1 JSON-RPC client
-func NewLazyEth1Client(executionEngineAddress string) *Client {
+// NewEth1Client returns an uninitialized eth1 JSON-RPC client.
+func NewLazyEth1Client(addr string) *Client {
 	return &Client{
 		eth1Cl:                 nil,
-		executionEngineAddress: executionEngineAddress,
+		executionEngineAddress: addr,
 	}
 }
 
 // Client wraps a eth1 client
 type Client struct {
+	eth1ClMu               sync.RWMutex
 	eth1Cl                 *ethclient.Client
 	executionEngineAddress string
 }
 
-// maybeInitializeClient initializes the eth1 client if not initialized
+// maybeInitializeClient initializes the eth1 client if not initialized.
 func (cl *Client) maybeInitializeClient() error {
+	// faster than acquiring a write lock
+    cl.eth1ClMu.RLock()
+    if cl.eth1Cl != nil {
+        cl.eth1ClMu.RUnlock()
+        return nil
+    }
+    cl.eth1ClMu.RUnlock()
+
+    // need write lock to initialize
+    cl.eth1ClMu.Lock()
+    defer cl.eth1ClMu.Unlock()
+
+	// double-check
+    if cl.eth1Cl != nil {
+        return nil
+    }
+
 	if cl.eth1Cl == nil {
 		err := cl.initializeClient()
 		if err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
-// initializeClient initializes the eth1 client
+// initializeClient initializes the eth1 client. This function is not thread safe.
 func (cl *Client) initializeClient() error {
 	eth1Cl, err := ethclient.Dial(cl.executionEngineAddress)
 	if err != nil {
@@ -57,7 +78,7 @@ func (cl *Client) initializeClient() error {
 	return nil
 }
 
-// VerifySmartContractBasedSignature returns true if sig is a valid signature of hash according to ERC-1271
+// VerifySmartContractBasedSignature returns true if sig is a valid signature of hash according to ERC-1271.
 func (cl *Client) VerifySmartContractBasedSignature(contractAddress string, hash [32]byte, sig []byte) (bool, error) {
 	err := cl.maybeInitializeClient()
 	if err != nil {
@@ -75,6 +96,6 @@ func (cl *Client) VerifySmartContractBasedSignature(contractAddress string, hash
 	if err != nil {
 		return false, err
 	}
-	
+
 	return result == ERC1271_MAGIC_VALUE, nil
 }
