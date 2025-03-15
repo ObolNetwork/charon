aceaa132776efb273dee293a7f1b3f9212a6161c
momantech <cuimoman@qq.com>
2025-03-15 10:45:30 +0800

    *: make function comment match function name
    Signed-off-by: momantech <cuimoman@qq.com>

--- eth2util/helpers.go
+++ eth2util/helpers.go
@@ -80,7 +80,7 @@ func ChecksumAddress(address string) (string, error) {
 	return checksumAddressBytes(b), nil
 }
 
-// ChecksumAddress returns an EIP55-compliant 0xhex representation of the binary ethereum address.
+// checksumAddressBytes returns an EIP55-compliant 0xhex representation of the binary ethereum address.
 func checksumAddressBytes(addressBytes []byte) string {
 	hexAddr := hex.EncodeToString(addressBytes)
 
