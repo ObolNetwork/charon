1e8be45afed3a967c4418e45b7be704fe5ebd360
Diogo Santos <diogo@obol.tech>
2025-08-29 11:22:14 +0100

    Add block and block gossip sse event and fix event timestamp

--- app/sse/metrics.go
+++ app/sse/metrics.go
@@ -21,7 +21,7 @@ var (
 		Subsystem: "beacon_node",
 		Name:      "sse_head_delay",
 		Help:      "Delay in seconds between slot start and head update, supplied by beacon node's SSE endpoint. Values between 8s and 12s for Ethereum mainnet are considered safe.",
-		Buckets:   []float64{4, 6, 8, 10, 12, 16, 20},
+		Buckets:   []float64{2, 4, 6, 8, 10, 12},
 	}, []string{"addr"})
 
 	sseChainReorgDepthHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
@@ -30,5 +30,21 @@ var (
 		Name:      "sse_chain_reorg_depth",
 		Help:      "Chain reorg depth, supplied by beacon node's SSE endpoint",
 		Buckets:   []float64{1, 2, 4, 6, 8, 16},
+	}, []string{"addr"})
+
+	sseBlockGossipHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "app",
+		Subsystem: "beacon_node",
+		Name:      "sse_block_gossip",
+		Help:      "Block reception via gossip delay, supplied by beacon node's SSE endpoint. Values between 0s and 4s for Ethereum mainnet are considered safe",
+		Buckets:   []float64{0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 8, 10, 12},
+	}, []string{"addr"})
+
+	sseBlockHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "app",
+		Subsystem: "beacon_node",
+		Name:      "sse_block",
+		Help:      "Block imported into fork choice delay, supplied by beacon node's SSE endpoint. Values between 0s and 4s for Ethereum mainnet are considered safe",
+		Buckets:   []float64{0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 8, 10, 12},
 	}, []string{"addr"})
 )
