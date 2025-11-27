978418b9956b489f64274cabbc80bcf068ee098f
Diogo Santos <59585571+DiogoSantoss@users.noreply.github.com>
2025-11-27 15:25:45 +0000

    *: cherry picking blinded metric (#4129)
    Cherry-picking #4117 for `v1.7.2`
    
    category: misc
    ticket: none

---
+++ core/fetcher/metrics.go
@@ -0,0 +1,18 @@
+// Copyright © 2022-2025 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
+
+package fetcher
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/obolnetwork/charon/app/promauto"
+)
+
+var (
+	proposalBlindedGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "core",
+		Subsystem: "fetcher",
+		Name:      "proposal_blinded",
+		Help:      "Whether the fetched proposal was blinded (1) or local (0)",
+	})
+)
