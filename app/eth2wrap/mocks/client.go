e937b08adaa7e3273ebccfdb564589ec226f0162
Kaloyan Tanev <kalo@obol.tech>
2026-01-20 14:55:27 +0200

    Add duties cache to vmock

--- app/eth2wrap/mocks/client.go
+++ app/eth2wrap/mocks/client.go
@@ -774,32 +774,12 @@ func (_m *Client) ProposerDuties(ctx context.Context, opts *api.ProposerDutiesOp
 
 // ProposerDutiesCache provides a mock function with given fields: _a0, _a1, _a2
 func (_m *Client) ProposerDutiesCache(_a0 context.Context, _a1 phase0.Epoch, _a2 []phase0.ValidatorIndex) ([]*v1.ProposerDuty, error) {
-	ret := _m.Called(_a0, _a1, _a2)
-
-	if len(ret) == 0 {
-		panic("no return value specified for ProposerDutiesCache")
+	r0, err := _m.ProposerDuties(_a0, &api.ProposerDutiesOpts{Epoch: _a1, Indices: _a2})
+	if err != nil {
+		return nil, err
 	}
 
-	var r0 []*v1.ProposerDuty
-	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, phase0.Epoch, []phase0.ValidatorIndex) ([]*v1.ProposerDuty, error)); ok {
-		return rf(_a0, _a1, _a2)
-	}
-	if rf, ok := ret.Get(0).(func(context.Context, phase0.Epoch, []phase0.ValidatorIndex) []*v1.ProposerDuty); ok {
-		r0 = rf(_a0, _a1, _a2)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]*v1.ProposerDuty)
-		}
-	}
-
-	if rf, ok := ret.Get(1).(func(context.Context, phase0.Epoch, []phase0.ValidatorIndex) error); ok {
-		r1 = rf(_a0, _a1, _a2)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
+	return r0.Data, nil
 }
 
 // Proxy provides a mock function with given fields: ctx, req
