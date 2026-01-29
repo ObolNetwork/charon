2f4e247f74eaaca5c7c81a5d073ffa334b46c54b
Kaloyan Tanev <kalo@obol.tech>
2026-01-29 14:14:21 +0200

    Add debug logs on reorgs

--- app/eth2wrap/cache.go
+++ app/eth2wrap/cache.go
@@ -15,6 +15,8 @@ import (
 
 	"github.com/obolnetwork/charon/app/errors"
 	"github.com/obolnetwork/charon/app/featureset"
+	"github.com/obolnetwork/charon/app/log"
+	"github.com/obolnetwork/charon/app/z"
 )
 
 // dutiesCacheTrimThreshold is the number of epochs after which duties are trimmed from the cache.
@@ -281,7 +283,7 @@ func (c *DutiesCache) UpdateCacheIndices(_ context.Context, indices []eth2p0.Val
 // InvalidateCache handles chain reorg, invalidating cached duties.
 // The epoch parameter indicates at which epoch the reorg led us to.
 // Meaning, we should invalidate all duties prior to that epoch.
-func (c *DutiesCache) InvalidateCache(_ context.Context, epoch eth2p0.Epoch) {
+func (c *DutiesCache) InvalidateCache(ctx context.Context, epoch eth2p0.Epoch) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -308,7 +310,10 @@ func (c *DutiesCache) InvalidateCache(_ context.Context, epoch eth2p0.Epoch) {
 	}
 
 	if invalidated {
+		log.Debug(ctx, "Reorg occurred through epoch transition, invalidating duties cache", z.U64("reorged_back_to_epoch", uint64(epoch)))
 		invalidatedCacheDueReorgCount.WithLabelValues("validators").Inc()
+	} else {
+		log.Debug(ctx, "Reorg occurred, but it was not through epoch transition, duties cache is not invalidated", z.U64("reorged_epoch", uint64(epoch)))
 	}
 }
 
