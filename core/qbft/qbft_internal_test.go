04bba5423fceb70e1927c4be9b493c593bb078b4
kalo <24719519+KaloyanTanev@users.noreply.github.com>
2026-03-11 10:44:30 +0100

    core: abundant QBFT logging (#4380)
    Abundant QBFT debug logging as an attempt to diagnose failing attestations.
    
    category: misc
    ticket: none

--- core/qbft/qbft_internal_test.go
+++ core/qbft/qbft_internal_test.go
@@ -332,6 +332,13 @@ func testQBFT(t *testing.T, test test) {
 			t.Logf("Unjust: %#v", msg)
 			cancel()
 		},
+		LogDebug: func(_ context.Context, instance int64, process int64, msg Msg[int64, int64], logMsg string) {
+			if test.Fuzz {
+				return // Ignore debug messages when fuzzing.
+			}
+
+			t.Logf("Debug: %s - %#v", logMsg, msg)
+		},
 		Nodes:     n,
 		FIFOLimit: fifoLimit,
 	}
@@ -747,4 +754,5 @@ var noopDef = Definition[int64, int64]{
 	LogUponRule:    func(context.Context, int64, int64, int64, Msg[int64, int64], UponRule) {},
 	LogRoundChange: func(context.Context, int64, int64, int64, int64, UponRule, []Msg[int64, int64]) {},
 	LogUnjust:      func(context.Context, int64, int64, Msg[int64, int64]) {},
+	LogDebug:       func(context.Context, int64, int64, Msg[int64, int64], string) {},
 }
